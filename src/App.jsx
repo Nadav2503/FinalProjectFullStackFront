551c3d99d03bdf0051f2cee8b1a7ce1473700d1b
Nadav2503 <nadavivon.re@gmail.com>
2024-11-19 16:07:48 +0200

    create main component so the layout will be with components only instead of design

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import About from "./pages/aboutPage"
-import Home from "./pages/HomePage"
+import React from 'react';
+import Layout from './components/layout/Layout';
+import Home from './pages/HomePage';
 
-
-function App() {
-
-
+export default function App() {
   return (
-    <About>
-    </About>
-  )
+    <Layout>
+      <Home />
+    </Layout>
+  );
 }
-
-export default App
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import { Box } from '@mui/material';
+import Main from './Main';
 
 export default function Layout({ children }) {
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                minHeight: '100vh',
-            }}
-        >
+        <>
             <Navbar />
-            <Box component="main" sx={{ flexGrow: 1, padding: '1rem' }}>
-                {children}
-            </Box>
+            <Main>{children}</Main>
             <Footer />
-        </Box>
+        </>
     );
 }
