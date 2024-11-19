ab964426d8483a77062867554f094fa7e2a66440
Nadav2503 <nadavivon.re@gmail.com>
2024-11-19 17:14:20 +0200

    add company name component to navbar

---
+++ src/components/layout/navbar/leftHeader/CompanyName.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+
+export default function CompanyName() {
+    return (
+        <Typography variant="h6" color="inherit">
+            Virtual Zoo
+        </Typography>
+    );
+}
