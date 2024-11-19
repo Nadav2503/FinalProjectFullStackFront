a4e97018330e1e7d0a4bb8848bbc26b2a7e4738c
Nadav2503 <nadavivon.re@gmail.com>
2024-11-19 18:13:37 +0200

    add state for opening the menu

--- src/components/layout/navbar/right/RightHeader.jsx
+++ src/components/layout/navbar/right/RightHeader.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box } from '@mui/material';
 import SwitchMode from './SwitchMode';
-import AvatarProfileImage from './Avatar';
-import AvatarMenu from './AvatarMenuAnchor';
+import AvatarComponent from './Avatar';
+import AvatarMenuAnchor from './AvatarMenuAnchor';
 
 
 export default function RightHeader() {
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleAvatarClick = (event) => {
+        setAnchorEl(event.currentTarget); // Open the menu by setting the anchor element
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null); // Close the menu by setting anchorEl to null
+    };
 
     return (
         <Box sx={{ display: 'flex', alignItems: 'center' }}>
 
             {/* Avatar */}
-            <AvatarProfileImage />
+            <AvatarComponent onClick={handleAvatarClick} />
+
             {/* Avatar Menu */}
-            <AvatarMenu />
+            <AvatarMenuAnchor anchorEl={anchorEl} onClose={handleMenuClose} />
             {/* SwitchMode */}
             <SwitchMode />
         </Box>
