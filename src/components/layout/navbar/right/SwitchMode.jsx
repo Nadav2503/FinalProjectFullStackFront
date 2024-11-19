f429dc8cd5589a887412d4964fffc0fa85106e97
Nadav2503 <nadavivon.re@gmail.com>
2024-11-19 22:43:45 +0200

    apply theme provider on switch mode

--- src/components/layout/navbar/right/SwitchMode.jsx
+++ src/components/layout/navbar/right/SwitchMode.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import { IconButton, Box } from '@mui/material';
 import { Brightness4, Brightness7 } from '@mui/icons-material';
+import { useTheme } from '../../../../providers/CustomThemeProvider';
 
 export default function SwitchMode() {
+  const { isDark, toggleDarkMode } = useTheme();
+
   return (
     <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%', gap: 2 }}>
-      {/* Light Mode Button */}
-      <IconButton sx={{ color: 'inherit' }}>
-        <Brightness7 />
-      </IconButton>
-
-      {/* Dark Mode Button */}
-      <IconButton sx={{ color: 'inherit' }}>
-        <Brightness4 />
+      <IconButton sx={{ color: 'inherit' }} onClick={toggleDarkMode}>
+        {isDark ? <Brightness7 /> : <Brightness4 />}
       </IconButton>
     </Box>
   );
