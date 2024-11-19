521d2e104db1cbf399b67f117222f7c9c666fa6d
Nadav2503 <nadavivon.re@gmail.com>
2024-11-19 21:03:09 +0200

    make the search bar to responsive search bar

--- src/components/layout/navbar/middle/Search.jsx
+++ src/components/layout/navbar/middle/Search.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import { TextField, Box } from '@mui/material';
+import React, { useState } from 'react';
+import { IconButton, Box, TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
+
 
 export default function Search() {
+    const [showSearch, setShowSearch] = useState(false);
+
+    const toggleSearch = () => {
+        setShowSearch(!showSearch);
+    };
     return (
-        <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' }}>
+            {/* Search Icon */}
+            <IconButton onClick={toggleSearch} color="inherit">
+                <SearchIcon />
+            </IconButton>
             <TextField
-                label="Search"
                 variant="outlined"
-                fullWidth
+                size="small"
+                placeholder="Search"
                 sx={{
                     maxWidth: 500,  // Make search bar a bit wider
                     borderRadius: 2,  // Rounded corners
