fe439db80a5ef617c2d11ecace89f955a92e8966
Nadav2503 <nadavivon.re@gmail.com>
2024-11-19 22:34:56 +0200

    add the theme provider to app.jsx to apply on all the project

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Router from "./routers/Router";
+import CustomThemeProvider from "./providers/CustomThemeProvider";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Router />
-      </Layout>
+      <CustomThemeProvider>
+        <Layout>
+          <Router />
+        </Layout>
+      </CustomThemeProvider>
     </BrowserRouter>
   );
 }
