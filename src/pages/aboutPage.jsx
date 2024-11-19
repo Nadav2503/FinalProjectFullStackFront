e48a057ebb20a3ada9dd35281947f798a911b869
Nadav2503 <nadavivon.re@gmail.com>
2024-11-19 14:59:59 +0200

    add design to the paragraph

--- src/pages/aboutPage.jsx
+++ src/pages/aboutPage.jsx
@@ -3,17 +3,49 @@ import { Container, Typography } from '@mui/material';
 
 export default function AboutPage() {
     return (
-        <Container>
-            <Typography>
+        <Container
+            maxWidth="md"
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                textAlign: 'center',
+                padding: '2rem',
+            }}
+        >
+            <Typography
+                variant="h3"
+                component="h1"
+                sx={{
+                    marginBottom: 2,
+                    fontSize: '2rem',
+                    fontWeight: 'bold',
+                }}
+            >
                 About the Virtual Zoo
             </Typography>
 
-            <Typography>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, deserunt. Cumque voluptates officia corporis deleniti ratione et eius quos commodi similique, eos illum repudiandae porro doloremque nihil laboriosam laudantium ducimus?
+            <Typography
+                variant="body1"
+                sx={{
+                    fontSize: '1.2rem',
+                    lineHeight: 1.6,
+                    maxWidth: '800px',
+                    marginBottom: 4,
+                }}
+            >
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, iusto amet, expedita temporibus nam quis ab repellendus error perspiciatis, consequuntur sed? Voluptate, consequuntur saepe magni odio sint sapiente rerum voluptates.
             </Typography>
 
-            <Typography>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum tempora laudantium ipsa alias corporis saepe commodi provident dolores! Ratione excepturi nam assumenda fuga ut. A vero inventore aperiam? Rerum, harum.
+            <Typography
+                variant="body2"
+                sx={{
+                    fontSize: '1rem',
+                    color: '#777',
+                }}
+            >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A magnam maiores, praesentium in eum facere, nesciunt, accusantium adipisci sapiente at voluptates consequuntur earum eligendi sint illo ratione! Voluptatibus, at aliquid.
             </Typography>
         </Container>
     );
