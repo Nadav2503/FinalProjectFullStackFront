369208d0aab93e3ac05cd3a97d5084dafe8f4e10
Nadav2503 <nadavivon.re@gmail.com>
2024-11-19 14:59:03 +0200

    create init about page with heading and 2 paragraph with(lorem)

---
+++ src/pages/aboutPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
+export default function AboutPage() {
+    return (
+        <Container>
+            <Typography>
+                About the Virtual Zoo
+            </Typography>
+
+            <Typography>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, deserunt. Cumque voluptates officia corporis deleniti ratione et eius quos commodi similique, eos illum repudiandae porro doloremque nihil laboriosam laudantium ducimus?
+            </Typography>
+
+            <Typography>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum tempora laudantium ipsa alias corporis saepe commodi provident dolores! Ratione excepturi nam assumenda fuga ut. A vero inventore aperiam? Rerum, harum.
+            </Typography>
+        </Container>
+    );
+}
