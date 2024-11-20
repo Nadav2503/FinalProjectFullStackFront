708cbfd5d56d1c0d9d212b2a6b7830db5facc533
Nadav2503 <nadavivon.re@gmail.com>
2024-11-20 12:47:27 +0200

    create error message component

---
+++ src/components/general/Error.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container, Grid, Typography } from '@mui/material';
+
+export default function Error({ errorMessage }) {
+    return (
+        <Container sx={{ textAlign: 'center', marginTop: 5 }}>
+            <Grid container spacing={2} justifyContent="center">
+                <Grid item xs={12} md={8}>
+                    <Typography variant="h5" color="error">
+                        Oops... something went wrong: {errorMessage}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <img
+                        width="100%"
+                        src="/images/error-image.png"
+                        alt="Error Illustration"
+                    />
+                </Grid>
+            </Grid>
+        </Container>
+    );
+}
