7411e075f564f7499d4b8fdbf3cc6501a7956d7c
Nadav2503 <nadavivon.re@gmail.com>
2024-11-20 08:43:00 +0200

    add comments to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import Layout from "./components/layout/Layout";
-import Router from "./routers/Router";
-import CustomThemeProvider from "./providers/CustomThemeProvider";
+import Layout from "./components/layout/Layout"; // Application layout component.
+import Router from "./routers/Router"; // Main routing component.
+import CustomThemeProvider from "./providers/CustomThemeProvider"; // Theme management provider.
 
+// Main application entry point.
 export default function App() {
   return (
     <BrowserRouter>
       <CustomThemeProvider>
+        {/* Wraps all components with theme and layout */}
         <Layout>
           <Router />
         </Layout>
