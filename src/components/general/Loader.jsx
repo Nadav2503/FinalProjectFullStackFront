bb7e910f7224de58a0026bb398c5b1d4f4096c71
Nadav2503 <nadavivon.re@gmail.com>
2024-11-20 12:51:26 +0200

    change the loader component to be like the other as structuer

--- src/components/general/Loader.jsx
+++ src/components/general/Loader.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { CircularProgress, Box } from '@mui/material';
 
-const Loader = () => {
+export default function Loader() {
     return (
         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
             <CircularProgress size={60} color="primary" />
         </Box>
-    );
-};
+    )
+}
 
-export default Loader;
