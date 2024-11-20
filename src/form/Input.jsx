46e2ce8ba28663eb5ee7a1fe14fdd7a711952500
Nadav2503 <nadavivon.re@gmail.com>
2024-11-20 16:09:08 +0200

    add Input component for form inputs fields

---
+++ src/form/Input.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { TextField, Grid } from "@mui/material";
+
+
+const Input = ({
+
+}) => {
+
+
+    return (
+        <Grid >
+            <TextField
+            />
+        </Grid>
+    );
+};
+
+export default Input;
