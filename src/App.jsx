5add3c231a83e5b9b413ddc1a132cca3c8b9e9dc
Nadav2503 <nadavivon.re@gmail.com>
2024-11-20 14:09:43 +0200

    wrap the app with the provider

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import { BrowserRouter } from "react-router-dom";
 import Layout from "./components/layout/Layout"; // Application layout component.
 import Router from "./routers/Router"; // Main routing component.
 import CustomThemeProvider from "./providers/CustomThemeProvider"; // Theme management provider.
+import SnackbarProvider from "./providers/SnackbarProvider";
 
 // Main application entry point.
 export default function App() {
   return (
     <BrowserRouter>
       <CustomThemeProvider>
-        {/* Wraps all components with theme and layout */}
-        <Layout>
-          <Router />
-        </Layout>
+        <SnackbarProvider>
+          {/* Wraps all components with theme and layout */}
+          <Layout>
+            <Router />
+          </Layout>
+        </SnackbarProvider>
       </CustomThemeProvider>
     </BrowserRouter>
   );
