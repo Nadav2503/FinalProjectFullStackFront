b9ab11e66832be94f0c95b9f9811b6d47d6cf0da
Nadav2503 <nadavivon.re@gmail.com>
2024-11-20 16:10:01 +0200

    add props to imputs + add ...rest to grid

--- src/form/Input.jsx
+++ src/form/Input.jsx
@@ -3,12 +3,20 @@ import { TextField, Grid } from "@mui/material";
 
 
 const Input = ({
-
+    variant = "outlined",
+    type = "text",
+    name,
+    data,
+    label,
+    required = true,
+    error,
+    onChange,
+    ...rest
 }) => {
 
 
     return (
-        <Grid >
+        <Grid item xs={12} {...rest}>
             <TextField
             />
         </Grid>
