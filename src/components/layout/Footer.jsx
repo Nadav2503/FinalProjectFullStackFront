fd2277d9d2e21bbd3c767dc528519572960ed5e1
Nadav2503 <nadavivon.re@gmail.com>
2024-11-20 10:32:59 +0200

    design footer with better colors, that match the color theme

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
 
-// Footer component displayed at the bottom of the page.
 export default function Footer() {
     return (
         <Box
             component="footer"
             sx={{
                 backgroundColor: 'primary.main', // Footer's background color from the theme.
-                color: 'white', // White text for contrast.
+                color: 'text.primary', // Text color based on the theme.
                 textAlign: 'center', // Centers the content horizontally.
                 padding: '1rem', // Adds padding inside the footer.
                 marginTop: 'auto', // Ensures the footer stays at the bottom of the page.
             }}
         >
-            {/* Displays copyright information dynamically for the current year */}
             <Typography variant="body2">
                 © {new Date().getFullYear()} Virtual Zoo. All rights reserved.
             </Typography>
