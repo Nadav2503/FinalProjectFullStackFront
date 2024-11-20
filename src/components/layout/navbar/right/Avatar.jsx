b319a4967af7ab8c762f1e71bcd62f9bff15ef10
Nadav2503 <nadavivon.re@gmail.com>
2024-11-20 08:25:42 +0200

    add comments to Avatar.jsx

--- src/components/layout/navbar/right/Avatar.jsx
+++ src/components/layout/navbar/right/Avatar.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { IconButton, Avatar } from '@mui/material';
 
+// Displays the user's avatar as a button. Clicking triggers the provided onClick handler.
 export default function AvatarProfileImage({ onClick }) {
     return (
+        // Icon button wraps the avatar for click functionality.
         <IconButton onClick={onClick} color="inherit" sx={{ ml: 2 }}>
+            {/* Avatar image displayed. Customize src for the desired user avatar. */}
             <Avatar alt="User Avatar" src="/images/avatar.png" />
         </IconButton>
     );
