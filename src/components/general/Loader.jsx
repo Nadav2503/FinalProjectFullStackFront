27a9fffec22a08cdd1b67b24029ff595332fd265
Nadav2503 <nadavivon.re@gmail.com>
2024-11-20 12:37:17 +0200

    add design for the loader

--- src/components/general/Loader.jsx
+++ src/components/general/Loader.jsx
@@ -3,8 +3,8 @@ import { CircularProgress, Box } from '@mui/material';
 
 const Loader = () => {
     return (
-        <Box>
-            <CircularProgress />
+        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <CircularProgress size={60} color="primary" />
         </Box>
     );
 };
