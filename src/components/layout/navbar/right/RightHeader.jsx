4679817bf7b9c880143cab3184cd14b81b648b23
Nadav2503 <nadavivon.re@gmail.com>
2024-11-20 08:26:45 +0200

    add comments to Header.jsx

--- src/components/layout/navbar/right/RightHeader.jsx
+++ src/components/layout/navbar/right/RightHeader.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import AvatarProfileImage from './Avatar';
-import AvatarMenu from './AvatarMenuAnchor';  // Updated to use the AvatarMenu
+import AvatarMenu from './AvatarMenuAnchor';
 import useAnchor from '../useAnchor';
 
-
+// Header section displaying the avatar and the associated menu.
 export default function RightHeader() {
+    // Custom hook for managing anchor element state.
     const { anchorEl, handleAnchorClick, handleAnchorClose } = useAnchor();
 
     return (
         <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative' }}>
-            {/* Avatar centered outside the menu */}
+            {/* Avatar icon button triggers the menu */}
             <AvatarProfileImage onClick={handleAnchorClick} />
 
-            {/* Avatar Menu */}
+            {/* Dropdown menu anchored to the avatar */}
             <AvatarMenu anchorEl={anchorEl} onClose={handleAnchorClose} />
         </Box>
     );
