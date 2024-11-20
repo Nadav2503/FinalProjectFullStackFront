dc55862a10deec856319342517e4de84a0955281
Nadav2503 <nadavivon.re@gmail.com>
2024-11-20 19:29:10 +0200

    add comments to Loader.jsx

--- src/general/Loader.jsx
+++ src/general/Loader.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { CircularProgress, Box } from '@mui/material';
 
+// Loader component: Displays a loading spinner in the center of the screen.
 export default function Loader() {
     return (
-        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <Box sx={{
+            display: 'flex', // Use flexbox layout
+            justifyContent: 'center', // Center the spinner horizontally
+            alignItems: 'center', // Center the spinner vertically
+            height: '100vh' // Full viewport height to center vertically
+        }}>
+            {/* CircularProgress: MUI's spinner component. */}
             <CircularProgress size={60} color="primary" />
         </Box>
-    )
+    );
 }
-
