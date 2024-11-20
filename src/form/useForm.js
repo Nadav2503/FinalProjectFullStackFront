724f021978ad837a68e1a00e00ba5c9c65e95006
Nadav2503 <nadavivon.re@gmail.com>
2024-11-20 16:14:04 +0200

    add callback to the funtion

--- src/form/useForm.js
+++ src/form/useForm.js
@@ -1,13 +1,25 @@
-import { useState } from "react";
-
+import { useCallback, useState } from "react";
+import Joi from "joi";
 
 export default function useForm(initialForm, schema, handleSubmit) {
     const [data, setData] = useState(initialForm);
     const [errors, setErrors] = useState({});
 
+    const validateProperty = useCallback(
+        (name, value) => {
+            let joiSchema = Joi.object({ [name]: schema[name] });
+            let { error } = joiSchema.validate({ [name]: value });
+            return error ? error.details[0].message : null;
+        },
+        [schema]
+    );
+
+
 
     return {
         data,
         errors,
+        setData,
+
     };
 }
