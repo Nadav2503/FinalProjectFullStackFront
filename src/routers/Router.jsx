bc940cee04e7cbddceb1d2aba54fedab48e69c64
Nadav2503 <nadavivon.re@gmail.com>
2024-11-20 08:41:26 +0200

    add comments to Router.jsx

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import ROUTES from "./routerModel";
+import ROUTES from "./routerModel"; // Centralized routing constants.
 
-import Home from "../pages/HomePage";
-import About from "../pages/AboutPage";
-import Error from "../pages/ErrorPage";
+import Home from "../pages/HomePage"; // Home page component.
+import About from "../pages/AboutPage"; // About page component.
+import Error from "../pages/ErrorPage"; // 404 Error page component.
 
+// Main Router component to define application routes.
 export default function Router() {
     return (
         <Routes>
+            {/* Route for the homepage */}
             <Route path={ROUTES.ROOT} element={<Home />} />
+
+            {/* Route for the About page */}
             <Route path={ROUTES.ABOUT} element={<About />} />
+
+            {/* Fallback route for non-existent paths */}
             <Route path="*" element={<Error />} />
         </Routes>
     );
