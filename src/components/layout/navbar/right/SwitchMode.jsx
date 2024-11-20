25ac23d1a3c99fbb2580af4e1abb363487fe7370
Nadav2503 <nadavivon.re@gmail.com>
2024-11-20 08:27:12 +0200

    add comments to SwitchMode.jsx

--- src/components/layout/navbar/right/SwitchMode.jsx
+++ src/components/layout/navbar/right/SwitchMode.jsx
@@ -3,12 +3,15 @@ import { IconButton, Box } from '@mui/material';
 import { Brightness4, Brightness7 } from '@mui/icons-material';
 import { useTheme } from '../../../../providers/CustomThemeProvider';
 
+// Toggles between dark and light modes.
 export default function SwitchMode() {
-  const { isDark, toggleDarkMode } = useTheme();
+  const { isDark, toggleDarkMode } = useTheme(); // Get theme state and toggle function.
 
   return (
     <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%', gap: 2 }}>
+      {/* Button to switch theme mode */}
       <IconButton sx={{ color: 'inherit' }} onClick={toggleDarkMode}>
+        {/* Display sun icon for light mode and moon icon for dark mode */}
         {isDark ? <Brightness7 /> : <Brightness4 />}
       </IconButton>
     </Box>
