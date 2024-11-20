e07777fc902c3d1daf66e32ae4cf9606c2887567
Nadav2503 <nadavivon.re@gmail.com>
2024-11-20 08:21:23 +0200

    add comments to LeftHeader.jsx

--- src/components/layout/navbar/left/LeftHeader.jsx
+++ src/components/layout/navbar/left/LeftHeader.jsx
@@ -5,7 +5,9 @@ import CompanyName from './CompanyName';
 export default function LeftHeader() {
     return (
         <div style={{ display: 'flex', alignItems: 'center', minWidth: 0 }}>
+            {/* Displays the logo on the left */}
             <Logo />
+            {/* Contains the company name with overflow handling */}
             <div style={{ minWidth: 0, overflow: 'hidden' }}>
                 <CompanyName />
             </div>
