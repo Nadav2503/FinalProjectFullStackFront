2503340db36c4982ab388d44cc17725f117a6233
Nadav2503 <nadavivon.re@gmail.com>
2024-11-21 14:30:24 +0200

    create add exhibit page

---
+++ src/Exhibit/pages/AddExhibitPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ExhibitForm from "../exhibit/components/ExhibitForm";;
+
+export default function AddExhibitPage() {
+
+    return (
+        <div style={{ padding: "20px" }}>
+            <ExhibitForm
+            />
+        </div>
+    );
+};
+
