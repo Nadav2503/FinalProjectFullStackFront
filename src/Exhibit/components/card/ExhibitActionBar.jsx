301e45cdb155f5a1566badc64644840cdd86ae60
Nadav2503 <nadavivon.re@gmail.com>
2024-11-21 10:51:28 +0200

    add the icons/buttons to the action bar

--- src/Exhibit/components/card/ExhibitActionBar.jsx
+++ src/Exhibit/components/card/ExhibitActionBar.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 
 export default function ExhibitCardActionBar({
-
+    exhibitId,
+    handleDelete,
+    handleEditExhibit,
+    handleFavorite,
+    isFavorite,
 }) {
     const actions = [
+        {
+
+            icon: <FavoriteIcon color={isFavorite ? 'error' : 'disabled'} />,
+            active: isFavorite,
+        },
+        {
+
+            icon: <EditIcon />,
+        },
+        {
+
+            icon: <DeleteIcon />,
+        },
     ];
 
     return <CardActionBar actions={actions} />;
