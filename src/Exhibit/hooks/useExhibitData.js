a57fcb69e3c22c025c3f5f10391164d130c39224
Nadav2503 <nadavivon.re@gmail.com>
2024-11-21 17:19:35 +0200

    add states to get all exhibit hook

--- src/Exhibit/hooks/useExhibitData.js
+++ src/Exhibit/hooks/useExhibitData.js
@@ -1,11 +1,16 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
+import { useSnack } from '../../providers/SnackbarProvider';
 
 
 export default function useExhibitData() {
+    const [exhibits, setExhibits] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const setSnack = useSnack();
 
     const fetchExhibits = useCallback(async () => {
 
-    }, []);
+    }, [setSnack]);
 
     return { fetchExhibits };
 }
