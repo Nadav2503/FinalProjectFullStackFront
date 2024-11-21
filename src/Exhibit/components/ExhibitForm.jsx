e84fd366ee5760af1cbfc3e0185b23a0227e95ef
Nadav2503 <nadavivon.re@gmail.com>
2024-11-21 12:56:15 +0200

    create exhibit form component

---
+++ src/Exhibit/components/ExhibitForm.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Form from "../../forms/components/Form";
+
+const ExhibitForm = ({
+
+}) => {
+    return (
+        <Form
+
+        >
+
+
+        </Form>
+    );
+};
+
+export default ExhibitForm;
