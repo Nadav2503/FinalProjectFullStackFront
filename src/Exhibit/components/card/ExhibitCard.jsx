a73febecd0c817a2b16af1c6a640473dc620370c
Nadav2503 <nadavivon.re@gmail.com>
2024-11-21 10:55:50 +0200

    create the exhibit card with all the components

---
+++ src/Exhibit/components/card/ExhibitCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Card from '../../../general/card/Card';
+import ExhibitCardHeader from './ExhibitCArdHeader';
+import ExhibitCardBody from './ExhibitCardBody';
+import ExhibitCardActionBar from './ExhibitActionBar';
+
+export default function ExhibitCard({
+    exhibit,
+    handleDelete,
+    handleEditExhibit,
+    handleFavorite,
+    isFavorite,
+}) {
+
+
+    return (
+        <Card>
+            <ExhibitCardHeader title={exhibit.name} />
+            <ExhibitCardBody
+                description={exhibit.description}
+                capacity={exhibit.capacity}
+                location={exhibit.location}
+                status={exhibit.status}
+            />
+
+
+            <ExhibitCardActionBar
+                exhibitId={exhibit._id}
+                handleDelete={handleDelete}
+                handleEditExhibit={handleEditExhibit}
+                handleFavorite={handleFavorite}
+                isFavorite={isFavorite}
+            />
+        </Card>
+    );
+}
