93917738162664f03f9d4f22a84e8a014a57c780
Nadav2503 <nadavivon.re@gmail.com>
2024-11-21 12:56:43 +0200

    add props of form

--- src/Exhibit/components/ExhibitForm.jsx
+++ src/Exhibit/components/ExhibitForm.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import Form from "../../forms/components/Form";
 
 const ExhibitForm = ({
-
+    onSubmit,
+    data,
 }) => {
     return (
         <Form
-
+            title={data._id ? "Edit Exhibit" : "Create Exhibit"}
+            onSubmit={onSubmit}
+            validateForm={() => true}
+            styles={{ maxWidth: "800px" }}
+            submitLabel={data._id ? "Update Exhibit" : "Create Exhibit"}
         >
 
 
