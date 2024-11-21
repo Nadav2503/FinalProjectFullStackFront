505ef55d7ba0e225385ce15670f4f154f1ce4803
Nadav2503 <nadavivon.re@gmail.com>
2024-11-21 18:34:21 +0200

    add validation for name

--- src/Exhibit/helpers/validationExhibit.js
+++ src/Exhibit/helpers/validationExhibit.js
@@ -1,6 +1,9 @@
 export const validateExhibit = (exhibit) => {
     const errors = {};
 
+    if (!exhibit.name || exhibit.name.trim().length < 3 || exhibit.name.trim().length > 256) {
+        errors.name = 'Name must be between 3 and 256 characters.';
+    }
 
 
     return errors;
