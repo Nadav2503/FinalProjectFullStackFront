8ffb0b1d217db64cdc3d4bfa2c42fea8f80ff272
Nadav2503 <nadavivon.re@gmail.com>
2024-11-21 10:41:59 +0200

    create exhibit card header component

---
+++ src/Exhibit/components/card/ExhibitCardHeader.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import CardHeader from '../../../general/card/CardHeader';
+
+export default function ExhibitCardHeader({ title }) {
+    return (
+        <CardHeader title={title} />
+    );
+}
