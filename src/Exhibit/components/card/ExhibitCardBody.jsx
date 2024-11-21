f51231ce4ecdc43919e17f05a552b4543014e612
Nadav2503 <nadavivon.re@gmail.com>
2024-11-21 10:43:52 +0200

    create exhibit card body component

---
+++ src/Exhibit/components/card/ExhibitCardBody.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import CardBody from '../../../general/card/CardBody';
+
+export default function ExhibitCardBody() {
+    return (
+        <CardBody content={content} />
+    );
+}
