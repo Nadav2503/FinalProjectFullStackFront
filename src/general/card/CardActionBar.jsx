4cbe5b0d3a669f9356ad11ed6c98c9868c84f559
Nadav2503 <nadavivon.re@gmail.com>
2024-11-21 09:35:44 +0200

    create CardActionBar component

---
+++ src/general/card/CardActionBar.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import { Box, Button } from '@mui/material';
+
+export default function CardActionBar({ actions }) {
+    return (
+        <Box
+
+        >
+
+            <Button
+
+            >
+
+            </Button>
+
+        </Box>
+    );
+}
