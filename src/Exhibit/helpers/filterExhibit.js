d1dcaf0e02e1994792b5fcf1a3082dc1958ca69d
Nadav2503 <nadavivon.re@gmail.com>
2024-11-21 18:36:58 +0200

    create filters by status, location and name

--- src/Exhibit/helpers/filterExhibit.js
+++ src/Exhibit/helpers/filterExhibit.js
@@ -1,3 +1,10 @@
 export const filterExhibitsByStatus = (exhibits, status) =>
     exhibits.filter((exhibit) => exhibit.status === status);
 
+export const filterExhibitsByLocation = (exhibits, location) =>
+    exhibits.filter((exhibit) => exhibit.location === location);
+
+export const searchExhibitsByName = (exhibits, searchTerm) =>
+    exhibits.filter((exhibit) =>
+        exhibit.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
