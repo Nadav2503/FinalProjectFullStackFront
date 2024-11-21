a4c2b3b2648f2453fe99da8e290bea4cbffb4a44
Nadav2503 <nadavivon.re@gmail.com>
2024-11-21 17:59:58 +0200

    create the request for the update animal in exhibit hook + add states

--- src/Exhibit/hooks/useUpdateAnimalsInExhibit.js
+++ src/Exhibit/hooks/useUpdateAnimalsInExhibit.js
@@ -1,11 +1,26 @@
 import { useCallback } from 'react';
+import { updateAnimalsInExhibit } from '../../services/ExhibitServiceApi';
+import { useSnack } from '../../providers/SnackbarProvider';
 
 export default function useUpdateAnimalsInExhibit() {
-
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const setSnack = useSnack();
 
     const handleUpdateAnimals = useCallback(async (id, animals) => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const data = await updateAnimalsInExhibit(id, animals);
+            setSnack('success', 'Animals updated in the exhibit successfully!');
+            return data; // Optionally return the updated exhibit with animals
+        } catch (err) {
+            setError(err.message);
+            setSnack('error', 'Failed to update animals in the exhibit');
+        } finally {
+            setIsLoading(false);
+        }
+    }, [setSnack]);
 
-    }, []);
-
-    return {};
+    return { isLoading, error, handleUpdateAnimals };
 }
