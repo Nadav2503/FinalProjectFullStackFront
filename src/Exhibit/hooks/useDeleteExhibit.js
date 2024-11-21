270da421b887cb639cb70f6b3a431c0c00e22fc1
Nadav2503 <nadavivon.re@gmail.com>
2024-11-21 17:40:58 +0200

    create useDeleteExhibit hook - delete exhibit

---
+++ src/Exhibit/hooks/useDeleteExhibit.js
@@ -0,0 +1,11 @@
+import { useCallback } from 'react';
+
+export default function useDeleteExhibit() {
+
+
+    const handleDeleteExhibit = useCallback(async (id) => {
+
+    }, []);
+
+    return { isLoading, error, handleDeleteExhibit };
+}
