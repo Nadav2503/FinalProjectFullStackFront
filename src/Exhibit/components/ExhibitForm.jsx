d82ad83554bf5262b763125082c9414c969142c7
Nadav2503 <nadavivon.re@gmail.com>
2024-11-21 12:57:52 +0200

    add the inputs for the from

--- src/Exhibit/components/ExhibitForm.jsx
+++ src/Exhibit/components/ExhibitForm.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Form from "../../forms/components/Form";
+import Input from "../../forms/components/Input";
+import { Grid, MenuItem, TextField } from "@mui/material";
 
 const ExhibitForm = ({
     onSubmit,
@@ -13,7 +15,39 @@ const ExhibitForm = ({
             styles={{ maxWidth: "800px" }}
             submitLabel={data._id ? "Update Exhibit" : "Create Exhibit"}
         >
+            <Input
 
+            />
+
+            <Grid >
+                <TextField
+
+                />
+            </Grid>
+
+            <Grid >
+                <TextField
+
+                >
+                    <MenuItem value="Africa">Africa</MenuItem>
+                    <MenuItem value="Asia">Asia</MenuItem>
+                    <MenuItem value="Europe">Europe</MenuItem>
+                    <MenuItem value="North America">North America</MenuItem>
+                    <MenuItem value="South America">South America</MenuItem>
+                    <MenuItem value="Australia">Australia</MenuItem>
+                    <MenuItem value="Antarctica">Antarctica</MenuItem>
+                </TextField>
+            </Grid>
+
+            <Grid >
+                <TextField
+
+                >
+                    <MenuItem value="open">Open</MenuItem>
+                    <MenuItem value="closed">Closed</MenuItem>
+                    <MenuItem value="under maintenance">Under Maintenance</MenuItem>
+                </TextField>
+            </Grid>
 
         </Form>
     );
