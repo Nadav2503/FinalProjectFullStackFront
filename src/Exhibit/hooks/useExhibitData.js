8f6f29e081ec14dadc47322a02efc1e858c8005a
Nadav2503 <nadavivon.re@gmail.com>
2024-11-21 17:18:12 +0200

    create useExhibitData hook - get all exhibits

---
+++ src/Exhibit/hooks/useExhibitData.js
@@ -0,0 +1,11 @@
+import { useCallback } from 'react';
+
+
+export default function useExhibitData() {
+
+    const fetchExhibits = useCallback(async () => {
+
+    }, []);
+
+    return { fetchExhibits };
+}
