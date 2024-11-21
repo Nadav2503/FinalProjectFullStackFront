cc0f26530fc056905777ec757901a1e70953355f
Nadav2503 <nadavivon.re@gmail.com>
2024-11-21 17:41:46 +0200

    add states to delete exhibit

--- src/Exhibit/hooks/useDeleteExhibit.js
+++ src/Exhibit/hooks/useDeleteExhibit.js
@@ -1,11 +1,14 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
+import { useSnack } from '../../providers/SnackbarProvider';
 
 export default function useDeleteExhibit() {
-
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const setSnack = useSnack();
 
     const handleDeleteExhibit = useCallback(async (id) => {
 
-    }, []);
+    }, [setSnack]);
 
     return { isLoading, error, handleDeleteExhibit };
 }
