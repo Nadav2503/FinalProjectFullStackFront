36978e1f76ead8afb5d5a7556337b3a223a632d3
Nadav2503 <nadavivon.re@gmail.com>
2024-11-21 17:04:36 +0200

    create getExhibits function

--- src/services/ExhibitServiceApi.js
+++ src/services/ExhibitServiceApi.js
@@ -3,3 +3,11 @@ import axios from "axios";
 // Set the base URL for your API
 const API_URL = "http://localhost:8181/Zoo/exhibits";
 
+export const getExhibits = async () => {
+    try {
+        const { data } = await axios.get(API_URL);
+        return data;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
