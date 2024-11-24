1196b1011d93c948abd328948b503cd385964cd0
Nadav2503 <nadavivon.re@gmail.com>
2024-11-24 10:55:08 +0200

    add page header to the page

--- src/Exhibit/pages/ExhibitListPage.jsx
+++ src/Exhibit/pages/ExhibitListPage.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Container } from "@mui/material";
+import PageHeader from "../../general/PageHeader";
 
 export default function ExhibitListPage() {
     return (
-        <div>ExhibitListPage</div>
-    )
+        <Container>
+            {/* Header Section */}
+            <PageHeader
+                title="Exhibits at Our Zoo"
+                subtitle="Browse the various exhibits and discover the incredible wildlife that calls our zoo home." />
+
+        </Container>
+    );
 }
