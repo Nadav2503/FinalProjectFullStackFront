0d519e145e1bda8e31c506188b3e939dbb2023b5
Nadav2503 <nadavivon.re@gmail.com>
2024-11-24 10:56:56 +0200

    add content - the exhibit cards + conditional rendering

--- src/Exhibit/pages/ExhibitListPage.jsx
+++ src/Exhibit/pages/ExhibitListPage.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Container } from "@mui/material";
 import PageHeader from "../../general/PageHeader";
+import Loader from "../../general/Loader";
+import Error from "../../general/Error";
+import ExhibitFeedback from "../components/ExhibitFeedback";
 
 export default function ExhibitListPage() {
     return (
@@ -9,7 +12,12 @@ export default function ExhibitListPage() {
             <PageHeader
                 title="Exhibits at Our Zoo"
                 subtitle="Browse the various exhibits and discover the incredible wildlife that calls our zoo home." />
-
+            {/* Content Section */}
+            {isLoading && <Loader />} {/* Show Loader while fetching data */}
+            {error && <Error errorMessage={error} />} {/* Show Error if there's an issue */}
+            {!isLoading && !error && (
+                <ExhibitFeedback exhibits={exhibits} isLoading={isLoading} error={error} />
+            )}
         </Container>
     );
 }
