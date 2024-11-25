c49d18f434e66adbfd3a990a2de4fc375cb4fa8d
Nadav2503 <nadavivon.re@gmail.com>
2024-11-25 10:32:46 +0200

    add the details to the exhibitDetails page

--- src/Exhibit/pages/ExhibitDetailsPage.jsx
+++ src/Exhibit/pages/ExhibitDetailsPage.jsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import React from "react";
 
-export default function ExhibitDetailsPage() {
+import { Box, Typography, Divider, Container } from "@mui/material";
+
+import PageHeader from "../../general/PageHeader";
+
+
+export default function ExhibitDetailPage() {
+
+
     return (
-        <div>ExhibitDetailsPage</div>
-    )
+        <Container>
+            {/* Page Header */}
+            <PageHeader
+                title={exhibit.name}
+                subtitle={exhibit.description}
+            />
+
+            {/* Exhibit Details Section */}
+            <Box
+
+            >
+                {/* Current Capacity with Pet Icon */}
+                <Box>
+
+                    <Typography variant="h6">
+                        Current Animals: {currentCapacity}
+                    </Typography>
+                </Box>
+
+                {/* Max Capacity */}
+                <Typography >
+                    Exhibit Capacity: {exhibit.capacity}
+                </Typography>
+
+                {/* Location with Icon */}
+                <Box >
+
+                    <Typography variant="h6">{exhibit.location}</Typography>
+                </Box>
+
+                {/* Status with Icon and updated colors */}
+                <Box >
+
+
+
+                    <Typography
+
+                    >
+                        {exhibit.status}
+                    </Typography>
+                </Box>
+
+                {/* Divider */}
+                <Divider />
+            </Box>
+
+            {/* Placeholder for Animals */}
+            <Box
+
+            >
+                <Typography >
+                    Animal cards will appear here once implemented.
+                </Typography>
+            </Box>
+        </Container>
+    );
 }
