13e660f2ff0a23c16c1502243e00705a37ad0a32
Nadav2503 <nadavivon.re@gmail.com>
2024-11-25 09:40:36 +0200

    improve the functionality of Exhibits + make the style look better

--- src/Exhibit/components/Exhibits.jsx
+++ src/Exhibit/components/Exhibits.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import { Container } from '@mui/material';
-import ExhibitCard from './card/ExhibitCard';
+import React from "react";
+import ExhibitCard from "./card/ExhibitCard";
+import { Container } from "@mui/material";
 
 export default function Exhibits({ exhibits, handleDelete, handleEditExhibit }) {
+
     return (
-        <Container sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-            {exhibits.map((exhibit) => (
-                <ExhibitCard
-                    key={exhibit._id}
-                    exhibit={exhibit}
-                    handleDelete={handleDelete}
-                    handleEditExhibit={handleEditExhibit}
-                />
-            ))}
+        <Container sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center", gap: 1 }
+        }>
+            {
+                exhibits.map((exhibit) => (
+                    <ExhibitCard
+                        exhibit={exhibit}
+                        key={exhibit._id}
+                        handleDelete={handleDelete}
+                        handleEditExhibit={handleEditExhibit}
+                    />
+                ))
+            }
         </Container>
     );
 }
