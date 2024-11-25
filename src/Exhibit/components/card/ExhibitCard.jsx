52fab19fc46224cd71cf2458ee8c8f1ce01d1a48
Nadav2503 <nadavivon.re@gmail.com>
2024-11-25 09:39:02 +0200

    remove the style of card since, there is no need for that + move the card action area to only wrap the body

--- src/Exhibit/components/card/ExhibitCard.jsx
+++ src/Exhibit/components/card/ExhibitCard.jsx
@@ -6,12 +6,11 @@ import ExhibitCardBody from './ExhibitCardBody';
 import ExhibitActionBar from './ExhibitActionBar';
 
 export default function ExhibitCard({ exhibit, handleDelete, handleEditExhibit }) {
-    console.log('Rendering ExhibitCard for:', exhibit);
 
     return (
-        <Card sx={{ maxWidth: 400, margin: 'auto', boxShadow: 3, borderRadius: 2 }}>
+        <Card>
+            < ExhibitCardHeader title={exhibit.name} />
             <CardActionArea>
-                <ExhibitCardHeader title={exhibit.name} />
                 <ExhibitCardBody
                     description={exhibit.description}
                     capacity={exhibit.capacity}
@@ -19,8 +18,7 @@ export default function ExhibitCard({ exhibit, handleDelete, handleEditExhibit }
                     status={exhibit.status}
                 />
             </CardActionArea>
-
-            <ExhibitActionBar
+            < ExhibitActionBar
                 exhibitId={exhibit._id}
                 handleDelete={handleDelete}
                 handleEditExhibit={handleEditExhibit}
