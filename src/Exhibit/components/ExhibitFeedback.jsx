ed9cb65fe92887c50833a58f89cd3757ada41082
Nadav2503 <nadavivon.re@gmail.com>
2024-11-25 09:41:41 +0200

    improve the functionality of ExhibitFeedback to prevent multiple rendering

--- src/Exhibit/components/ExhibitFeedback.jsx
+++ src/Exhibit/components/ExhibitFeedback.jsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import Loader from '../../general/Loader';
-import Error from '../../general/Error';
-import Exhibits from './Exhibits';
+import React from "react";
+import Exhibits from "./Exhibits";
+import Loader from "../../general/Loader";
+import Error from "../../general/Error";
 
-export default function ExhibitFeedback({
-    isLoading,
-    exhibits,
-    error,
-    handleDelete,
-    handleEditExhibit,
-}) {
+export default function ExhibitFeedback({ isLoading, exhibits, error, handleDelete, handleEditExhibit }) {
+
     if (isLoading) return <Loader />;
     if (error) return <Error errorMessage={error} />;
-    if (!exhibits || exhibits.length === 0) return <Error errorMessage="Oops... it seems there are no exhibits to display" />;
-    if (exhibits)
-        return (
-            <Exhibits
-                exhibits={exhibits}
-                handleDelete={handleDelete}
-                handleEditExhibit={handleEditExhibit}
-            />
-        );
-    return null;
+    if (!exhibits || exhibits.length === 0) return <Error errorMessage="Oops...no exhibits to display" />;
+
+    return (
+        <Exhibits
+            exhibits={exhibits}
+            handleDelete={handleDelete}
+            handleEditExhibit={handleEditExhibit}
+        />
+    );
 }
