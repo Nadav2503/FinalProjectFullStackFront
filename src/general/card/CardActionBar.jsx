02bd36f74efa67bdbe3c56e323e9e77d55e4ed2e
Nadav2503 <nadavivon.re@gmail.com>
2024-11-25 09:31:46 +0200

    improve the functionality of card action bar

--- src/general/card/CardActionBar.jsx
+++ src/general/card/CardActionBar.jsx
@@ -9,26 +9,28 @@ export default function CardActionBar({ actions = [] }) {
             className="card-actions"
             sx={{
                 display: 'flex',
-                flexDirection: isSmallScreen ? 'column' : 'row', // Stack icons vertically on small screens
+                flexDirection: isSmallScreen ? 'column' : 'row',
                 justifyContent: 'space-between',
                 gap: 1,
                 padding: 2,
                 width: '100%',
-            }}
+            }
+            }
         >
-            {actions.map((action, index) => (
-                <IconButton
-                    key={index}
-                    onClick={action.onClick}
-                    color={action.active ? 'primary' : 'default'} // Optional: Customize color based on active state
-                    sx={{
-                        width: isSmallScreen ? '100%' : 'auto', // Full width on small screens
-                        textAlign: 'center', // Centers the button text
-                    }}
-                >
-                    {action.icon} {/* Render the icon passed in the action */}
-                </IconButton>
-            ))}
+            {
+                actions.map((action, index) => (
+                    <IconButton
+                        key={index}
+                        onClick={action.onClick}
+                        color={action.active ? 'primary' : 'default'}
+                        sx={{
+                            width: isSmallScreen ? '100%' : 'auto',
+                            textAlign: 'center',
+                        }}
+                    >
+                        {action.icon}
+                    </IconButton>
+                ))}
         </Box>
     );
 }
