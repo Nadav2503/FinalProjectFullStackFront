685c20c6a2868734a42844c821e965dfcdb1763c
Nadav2503 <nadavivon.re@gmail.com>
2024-11-25 09:35:14 +0200

    change the return to one line

--- src/Exhibit/components/card/ExhibitActionBar.jsx
+++ src/Exhibit/components/card/ExhibitActionBar.jsx
@@ -3,11 +3,7 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CardActionBar from '../../../general/card/CardActionBar';
 
-export default function ExhibitCardActionBar({
-    exhibitId,
-    handleDelete,
-    handleEditExhibit,
-}) {
+export default function ExhibitActionBar({ exhibitId, handleDelete, handleEditExhibit }) {
     const actions = [
         {
             onClick: () => handleEditExhibit(exhibitId),
