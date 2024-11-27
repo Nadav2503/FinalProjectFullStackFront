1ed185f4ef80c1b83cb31f9c205e2a1614ab42ea
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 17:33:15 +0200

    add favorite icon and function to action array

--- src/animal/components/card/AnimalActionBar.jsx
+++ src/animal/components/card/AnimalActionBar.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
-
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import CardActionBar from '../../../general/card/CardActionBar';
 
-export default function AnimalActionBar({ animalId, handleDelete, handleEditAnimal }) {
+export default function AnimalActionBar({ animalId, handleDelete, handleEditAnimal, handleFavoriteToggle }) {
     const actions = [
         {
             onClick: () => handleEditAnimal(animalId), // Call handleEditAnimal when the edit icon is clicked
@@ -14,7 +14,10 @@ export default function AnimalActionBar({ animalId, handleDelete, handleEditAnim
             onClick: () => handleDelete(animalId), // Call handleDelete when the delete icon is clicked
             icon: <DeleteIcon />,
         },
-
+        {
+            onClick: () => handleFavoriteToggle(animalId), // Call handleFavoriteToggle when the favorite icon is clicked
+            icon: <FavoriteIcon />,
+        },
     ];
 
     return <CardActionBar actions={actions} />;
