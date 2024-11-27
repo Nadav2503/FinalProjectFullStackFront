ac0449ce9b2c8e016f9a188786452d85ff318819
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 17:32:51 +0200

    add delete icon and function to action array

--- src/animal/components/card/AnimalActionBar.jsx
+++ src/animal/components/card/AnimalActionBar.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 import CardActionBar from '../../../general/card/CardActionBar';
 
-export default function AnimalActionBar({ animalId, handleEditAnimal }) {
+export default function AnimalActionBar({ animalId, handleDelete, handleEditAnimal }) {
     const actions = [
         {
             onClick: () => handleEditAnimal(animalId), // Call handleEditAnimal when the edit icon is clicked
             icon: <EditIcon />,
+        },
+        {
+            onClick: () => handleDelete(animalId), // Call handleDelete when the delete icon is clicked
+            icon: <DeleteIcon />,
         },
 
     ];
