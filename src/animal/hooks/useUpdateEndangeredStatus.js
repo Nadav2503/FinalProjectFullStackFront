3b784fafa1b634e2ce13421602f5b9712d553ad4
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 16:27:25 +0200

    add use effect function to update animal endangered status

--- src/animal/hooks/useUpdateEndangeredStatus.js
+++ src/animal/hooks/useUpdateEndangeredStatus.js
@@ -1,12 +1,25 @@
 import { useState } from "react";
-
+import { updateEndangeredStatus } from "../../services/AnimalServiceApi";
 
 const useUpdateEndangeredStatus = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
+    const updateStatus = async (id, status) => {
+        setLoading(true);
+        setError(null);
 
-    return { loading, error };
+        try {
+            const data = await updateEndangeredStatus(id, status);
+            return data;
+        } catch (err) {
+            setError(err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return { updateStatus, loading, error };
 };
 
 export default useUpdateEndangeredStatus;
