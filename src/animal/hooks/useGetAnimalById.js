83dd8f55835e91fd25940aad00cc8a2ea006e618
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 16:16:34 +0200

    create use effect function to get specific animal

--- src/animal/hooks/useGetAnimalById.js
+++ src/animal/hooks/useGetAnimalById.js
@@ -1,11 +1,29 @@
 import { useState, useEffect } from "react";
+import { getAnimalById } from "../../services/AnimalServiceApi";
 
 const useGetAnimalById = (id) => {
     const [animal, setAnimal] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    return { loading, error };
+    useEffect(() => {
+        if (!id) return;
+
+        const fetchAnimal = async () => {
+            try {
+                const data = await getAnimalById(id);
+                setAnimal(data);
+            } catch (err) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchAnimal();
+    }, [id]);
+
+    return { animal, loading, error };
 };
 
 export default useGetAnimalById;
