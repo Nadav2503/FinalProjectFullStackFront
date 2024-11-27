1f9e0dac9831a67baa9c1759680631c1a8193732
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 10:00:05 +0200

    connect the edit page to exhibit action bar

--- src/Exhibit/components/card/ExhibitActionBar.jsx
+++ src/Exhibit/components/card/ExhibitActionBar.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CardActionBar from '../../../general/card/CardActionBar';
+import { useNavigate } from 'react-router-dom';
 
-export default function ExhibitActionBar({ exhibitId, handleDelete, handleEditExhibit }) {
+export default function ExhibitActionBar({ exhibitId, handleDelete, }) {
+    const navigate = useNavigate();
     const actions = [
         {
-            onClick: () => handleEditExhibit(exhibitId),
+            onClick: () => navigate(`/edit-exhibit/${exhibitId}`),
             icon: <EditIcon />,
         },
         {
