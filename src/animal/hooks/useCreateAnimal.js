ddc0b27d51a350d4d545015596c23b1d3d20a3da
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 16:20:19 +0200

    add use states

--- src/animal/hooks/useCreateAnimal.js
+++ src/animal/hooks/useCreateAnimal.js
@@ -1,3 +1,10 @@
-const useCreateAnimal = () => { };
+import { useState } from "react";
+import { createAnimal } from "../services/animalService";
+
+const useCreateAnimal = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    return { create, loading, error };
+};
 
 export default useCreateAnimal;
