30cbcef6fa994fc3a05ca40c632f0dc48b6f1527
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 18:24:17 +0200

    add Form comnponent to AnimalForm.jsx

---
+++ src/animal/components/AnimalForm.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Form from "../../form/Form";
+
+
+const AnimalForm = ({
+
+}) => {
+    return (
+        <Form
+
+        >
+
+        </Form>
+    );
+};
+
+export default AnimalForm;
