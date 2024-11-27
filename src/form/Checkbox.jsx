d8b04d24f2fac008e4a18794fefe230d1ad93269
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 18:17:18 +0200

    create grid with typography

---
+++ src/form/Checkbox.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const CheckboxField = ({
+
+}) => {
+    return (
+        <Grid >
+            <Typography >
+
+            </Typography>
+
+        </Grid>
+    );
+};
+
+export default CheckboxField;
