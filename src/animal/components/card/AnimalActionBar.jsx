28e52812755397fea868861eb482e9edcb102fe2
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 17:30:53 +0200

    use CardActionBar component in AnimalActionBar

---
+++ src/animal/components/card/AnimalActionBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import CardActionBar from '../../../general/card/CardActionBar';
+
+
+export default function AnimalActionBar() {
+
+
+    return <CardActionBar actions={actions} />;
+}
