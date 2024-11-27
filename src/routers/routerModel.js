be2ddad1a9295c6da94f07b498822a8c092b7885
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 10:01:36 +0200

    add add exhib it page and edit exhibit page to routerModel

--- src/routers/routerModel.js
+++ src/routers/routerModel.js
@@ -2,6 +2,8 @@ const ROUTES = {
     ROOT: "/", // Homepage route.
     ABOUT: "/about", // About page route.
     EXHIBITS: "/exhibits", // All exhibits page route.
+    ADDEXHIBITS: "/add-exhibit", // All exhibits page route.
+    EDITEXHIBITS: "/edit-exhibit/:id",
     EXHIBIT: "/exhibits/:exhibitId", // Specific exhibit page route.
 };
 
