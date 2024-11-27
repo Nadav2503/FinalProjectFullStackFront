c76386b73d52a2ebaeea9214d7306995528fb923
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 16:26:18 +0200

    add states

--- src/animal/hooks/useUpdateEndangeredStatus.js
+++ src/animal/hooks/useUpdateEndangeredStatus.js
@@ -1,6 +1,12 @@
+import { useState } from "react";
+
 
 const useUpdateEndangeredStatus = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
+
+    return { loading, error };
 };
 
 export default useUpdateEndangeredStatus;
