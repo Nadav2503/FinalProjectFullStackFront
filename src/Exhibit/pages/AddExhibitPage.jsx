409bab2fb06e69445effc18e55d0dc718640a26c
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 09:52:06 +0200

    add useForm to enable the use of ExhibitForm

--- src/Exhibit/pages/AddExhibitPage.jsx
+++ src/Exhibit/pages/AddExhibitPage.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import ExhibitForm from "../components/ExhibitForm";
+import initializeExhibit from "../helpers/initializeExhibit";
+
+import useForm from "../../form/useForm";
+import exhibitSchema from "../model/exhibitSchema";
 import { Container } from "@mui/material";
 
 export default function AddExhibitPage() {
+
+
+    const { data, errors, handleChange, validateForm, onSubmit } = useForm(
+        initializeExhibit,  // Initialize the form with an empty exhibit structure
+        exhibitSchema,      // Validate using the custom function
+        handleSubmit
+    );
+
+    console.log("Rendering AddExhibitPage with data:", data);
 
     return (
         <Container>
