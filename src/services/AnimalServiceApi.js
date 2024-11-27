a71025b111afb1474dc27c8b5e1cbd20aa69b8fc
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 14:20:51 +0200

    create getAnimals function

--- src/services/AnimalServiceApi.js
+++ src/services/AnimalServiceApi.js
@@ -2,3 +2,13 @@ import axios from "axios";
 
 // Set the base URL for your API
 const API_URL = "http://localhost:8181/Zoo/animals";
+
+// Get all animals
+export const getAnimals = async () => {
+    try {
+        const { data } = await axios.get(API_URL);
+        return data;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
