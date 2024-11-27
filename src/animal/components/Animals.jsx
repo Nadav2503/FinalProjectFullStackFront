5441f09ea0f652c833896c0b1a116acce7ea4796
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 17:58:27 +0200

    add animalCard component and wrap it with container

---
+++ src/animal/components/Animals.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import AnimalCard from "./card/AnimalCard";
+import { Container } from "@mui/material";
+
+export default function Animals() {
+    return (
+        <Container >
+            {
+
+                <AnimalCard
+
+                />
+
+            }
+        </Container>
+    );
+}
