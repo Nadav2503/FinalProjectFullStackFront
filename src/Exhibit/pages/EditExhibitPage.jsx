e0959d39356ef5f75b357c12e04b65ee90ff9a18
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 09:57:24 +0200

    add use form to edit exhibit page to enable the use of exhibit form

--- src/Exhibit/pages/EditExhibitPage.jsx
+++ src/Exhibit/pages/EditExhibitPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import { Container } from '@mui/material';
 import ExhibitForm from '../components/ExhibitForm';
+import useForm from '../../form/useForm';
 
 export default function EditExhibitPage() {
+    // Initialize form handling
+    const { data, errors, handleChange, validateForm, onSubmit, setData } = useForm(
+        exhibit || {}, // Initialize form with fetched exhibit data (or an empty object if not available)
+        exhibitSchema, // Schema to validate the form
+        handleSubmit // Form submission handler
+    );
+
     return (
         <Container>
             <ExhibitForm
