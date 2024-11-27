17e7264a8425fe04948ed7d171f452b8bbed91a6
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 16:35:31 +0200

    add normalize function + add use callback instead of use effect in create animal hook + add set snack for snackbar messages

--- src/animal/hooks/useCreateAnimal.js
+++ src/animal/hooks/useCreateAnimal.js
@@ -1,25 +1,32 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { createAnimal } from "../../services/AnimalServiceApi";
+import { useSnack } from "../../providers/SnackbarProvider";
+import normalizeAnimal from "../helpers/normalizeAnimal";
 
 const useCreateAnimal = () => {
+    const [animal, setAnimal] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+    const setSnack = useSnack();
 
-    const create = async (animal) => {
+    const handleCreateAnimal = useCallback(async (animalFromClient) => {
         setLoading(true);
         setError(null);
-
         try {
-            const data = await createAnimal(animal);
-            return data;
+            const normalizedAnimal = normalizeAnimal(animalFromClient)
+            const data = await createAnimal(normalizedAnimal);
+            setAnimal(data);
+            setSnack('success', 'Animal created successfully!');
         } catch (err) {
             setError(err.message);
+            setSnack('error', 'Failed to create animal');
         } finally {
             setLoading(false);
         }
-    };
+    }, [setSnack]);
 
-    return { create, loading, error };
+    return { animal, loading, error, handleCreateAnimal };
 };
 
 export default useCreateAnimal;
+
