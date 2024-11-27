2ff4060c43f5150ddc2a3a49ec306f789012ca4b
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 17:32:03 +0200

    add edit icon and function to action array

--- src/animal/components/card/AnimalActionBar.jsx
+++ src/animal/components/card/AnimalActionBar.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import EditIcon from '@mui/icons-material/Edit';
+
 import CardActionBar from '../../../general/card/CardActionBar';
 
+export default function AnimalActionBar({ animalId, handleEditAnimal }) {
+    const actions = [
+        {
+            onClick: () => handleEditAnimal(animalId), // Call handleEditAnimal when the edit icon is clicked
+            icon: <EditIcon />,
+        },
 
-export default function AnimalActionBar() {
-
+    ];
 
     return <CardActionBar actions={actions} />;
 }
