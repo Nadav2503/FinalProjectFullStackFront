6f33afdb9663a0033accfa39d390c88cdac0cf00
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 16:42:02 +0200

    add snackba messages + add use callback instead of use effect in get animal by id hook

--- src/animal/hooks/useGetAnimalById.js
+++ src/animal/hooks/useGetAnimalById.js
@@ -1,29 +1,28 @@
-import { useState, useEffect } from "react";
+import { useState, useCallback } from "react";
 import { getAnimalById } from "../../services/AnimalServiceApi";
+import { useSnack } from "../../providers/SnackbarProvider";
 
 const useGetAnimalById = (id) => {
     const [animal, setAnimal] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const setSnack = useSnack();
 
-    useEffect(() => {
-        if (!id) return;
+    const fetchAnimalById = useCallback(async () => {
+        try {
+            const data = await getAnimalById(id);
+            setAnimal(data);
+            setSnack('success', `Animal ${id} fetched successfully!`);
+        } catch (err) {
+            setError(err.message);
+            setSnack('error', `Failed to fetch animal ${id}`);
+        } finally {
+            setLoading(false);
+        }
+    }, []);
 
-        const fetchAnimal = async () => {
-            try {
-                const data = await getAnimalById(id);
-                setAnimal(data);
-            } catch (err) {
-                setError(err.message);
-            } finally {
-                setLoading(false);
-            }
-        };
 
-        fetchAnimal();
-    }, [id]);
-
-    return { animal, loading, error };
+    return { animal, loading, error, fetchAnimalById };
 };
 
 export default useGetAnimalById;
