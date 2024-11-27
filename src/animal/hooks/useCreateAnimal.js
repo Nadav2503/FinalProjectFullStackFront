e697b6d76bf4ac16570d865d10a18e63b2c57aa9
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 16:22:02 +0200

    create use effect function to create new animal

--- src/animal/hooks/useCreateAnimal.js
+++ src/animal/hooks/useCreateAnimal.js
@@ -1,9 +1,24 @@
 import { useState } from "react";
-import { createAnimal } from "../services/animalService";
+import { createAnimal } from "../../services/AnimalServiceApi";
 
 const useCreateAnimal = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+
+    const create = async (animal) => {
+        setLoading(true);
+        setError(null);
+
+        try {
+            const data = await createAnimal(animal);
+            return data;
+        } catch (err) {
+            setError(err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return { create, loading, error };
 };
 
