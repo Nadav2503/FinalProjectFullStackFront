ddbd99f54f89cb1803be0a2df660719de5ca3ce0
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 16:07:23 +0200

    add use states

--- src/animal/hooks/useGetAnimals.js
+++ src/animal/hooks/useGetAnimals.js
@@ -1,6 +1,9 @@
-
+import { useState } from "react";
 
 const useGetAnimals = () => {
-
-}
+    const [animals, setAnimals] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    return { loading, error };
+};
 export default useGetAnimals;
