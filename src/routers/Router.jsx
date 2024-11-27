33c7ec8c96bfc0c559d3a0a811675a842dac3fae
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 12:47:04 +0200

    improve the routerModel names + make the routes in Router more readable

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -1,36 +1,40 @@
+// Router.jsx
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import ROUTES from "./routerModel"; // Centralized routing constants.
+import ROUTES from "./routerModel"; // Centralized routing constants
 
-import Home from "../pages/HomePage"; // Home page component.
-import About from "../pages/AboutPage"; // About page component.
-import Error from "../pages/ErrorPage"; // 404 Error page component.
-import ExhibitListPage from "../Exhibit/pages/ExhibitListPage";
-import ExhibitDetailsPage from "../Exhibit/pages/ExhibitDetailsPage";
-import AddExhibitPage from "../Exhibit/pages/AddExhibitPage";
-import EditExhibitPage from "../Exhibit/pages/EditExhibitPage";
+import Home from "../pages/HomePage"; // Home page component
+import About from "../pages/AboutPage"; // About page component
+import Error from "../pages/ErrorPage"; // 404 Error page component
+import ExhibitListPage from "../Exhibit/pages/ExhibitListPage"; // All exhibits page
+import ExhibitDetailsPage from "../Exhibit/pages/ExhibitDetailsPage"; // Specific exhibit page
+import AddExhibitPage from "../Exhibit/pages/AddExhibitPage"; // Add new exhibit page
+import EditExhibitPage from "../Exhibit/pages/EditExhibitPage"; // Edit an exhibit page
 
-// Main Router component to define application routes.
+// Main Router component to define application routes
 export default function Router() {
     return (
         <Routes>
             {/* Route for the homepage */}
-            < Route path={ROUTES.ROOT} element={< Home />} />
+            <Route path={ROUTES.ROOT} element={<Home />} />
 
             {/* Route for the About page */}
-            <Route path={ROUTES.ABOUT} element={< About />} />
+            <Route path={ROUTES.ABOUT} element={<About />} />
 
             {/* Route for the exhibits page */}
-            <Route path={ROUTES.EXHIBITS} element={< ExhibitListPage />} />
+            <Route path={ROUTES.EXHIBITS} element={<ExhibitListPage />} />
 
-            < Route path={ROUTES.ADDEXHIBITS} element={< AddExhibitPage />} />
-            < Route path={ROUTES.EDITEXHIBITS} element={< EditExhibitPage />} />
+            {/* Route for adding a new exhibit */}
+            <Route path={ROUTES.ADD_EXHIBIT} element={<AddExhibitPage />} />
 
-            {/* Route for the exhibit page */}
-            <Route path={ROUTES.EXHIBIT} element={< ExhibitDetailsPage />} />
+            {/* Route for editing an exhibit */}
+            <Route path={`${ROUTES.EDIT_EXHIBIT}/:exhibitId`} element={<EditExhibitPage />} />
+
+            {/* Route for a specific exhibit details page */}
+            <Route path={`${ROUTES.EXHIBIT_INFO}/:exhibitId`} element={<ExhibitDetailsPage />} />
 
             {/* Fallback route for non-existent paths */}
-            <Route path="*" element={< Error />} />
+            <Route path={ROUTES.ERROR} element={<Error />} />
         </Routes>
     );
 }
--- src/routers/routerModel.js
+++ src/routers/routerModel.js
@@ -1,10 +1,11 @@
 const ROUTES = {
-    ROOT: "/", // Homepage route.
-    ABOUT: "/about", // About page route.
-    EXHIBITS: "/exhibits", // All exhibits page route.
-    ADDEXHIBITS: "/add-exhibit", // All exhibits page route.
-    EDITEXHIBITS: "/edit-exhibit/:id",
-    EXHIBIT: "/exhibits/:exhibitId", // Specific exhibit page route.
+    ROOT: "/", // Homepage route
+    ABOUT: "/about", // About page route
+    EXHIBITS: "/exhibits", // All exhibits page route
+    EXHIBIT_INFO: "/exhibit-info", // All exhibits page route
+    ADD_EXHIBIT: "/add-exhibit", // Add new exhibit route
+    EDIT_EXHIBIT: "/edit-exhibit", // Edit exhibit route (without dynamic id here)
+    ERROR: "*", // Fallback route for non-existent paths
 };
 
 export default ROUTES;
