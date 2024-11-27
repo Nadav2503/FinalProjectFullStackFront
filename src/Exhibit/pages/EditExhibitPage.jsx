c1409941551613c531fbe31b1d3520587a916d46
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 09:56:05 +0200

    add exhibitForm to the edit exhibit page

--- src/Exhibit/pages/EditExhibitPage.jsx
+++ src/Exhibit/pages/EditExhibitPage.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Container } from '@mui/material';
+import ExhibitForm from '../components/ExhibitForm';
 
 export default function EditExhibitPage() {
     return (
-        <div>EditExhibitPage</div>
-    )
+        <Container>
+            <ExhibitForm
+                title="Edit Exhibit"
+                submitLabel="Update Exhibit"
+                onSubmit={onSubmit}
+                validateForm={validateForm}
+                errors={errors}
+                data={data}
+                onInputChange={handleChange}
+            />
+        </Container>
+    );
 }
