43fdaa947c6c17a1725417e652424e86e64b9f28
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 16:38:28 +0200

    add use callback instead of use effect in delete animal hook + add set snack for snackbar messages

--- src/animal/hooks/useDeleteAnimal.js
+++ src/animal/hooks/useDeleteAnimal.js
@@ -1,26 +1,27 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { deleteAnimal } from "../../services/AnimalServiceApi";
-
+import { useSnack } from "../../providers/SnackbarProvider";
 
 const useDeleteAnimal = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+    const setSnack = useSnack();
 
-    const remove = async (id) => {
+    const handleDeleteAnimal = useCallback(async (id) => {
         setLoading(true);
         setError(null);
-
         try {
-            const data = await deleteAnimal(id);
-            return data;
+            await deleteAnimal(id);
+            setSnack('success', `Animal ${id} deleted successfully!`);
         } catch (err) {
             setError(err.message);
+            setSnack('error', `Failed to delete animal ${id}`);
         } finally {
             setLoading(false);
         }
-    };
+    }, [setSnack]);
 
-    return { remove, loading, error };
+    return { handleDeleteAnimal, loading, error };
 };
 
 export default useDeleteAnimal;
