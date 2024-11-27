8c6df87e9176189df5b6adb70728d54bb7f58826
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 16:15:45 +0200

    add use states

--- src/animal/hooks/useGetAnimalById.js
+++ src/animal/hooks/useGetAnimalById.js
@@ -1,3 +1,11 @@
-const useGetAnimalById = (id) => { };
+import { useState, useEffect } from "react";
+
+const useGetAnimalById = (id) => {
+    const [animal, setAnimal] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    return { loading, error };
+};
 
 export default useGetAnimalById;
