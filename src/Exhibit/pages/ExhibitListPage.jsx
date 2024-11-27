7a335d9e7296d893eb1c43ee0f095a5b808c6d36
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 09:27:31 +0200

    connect the add exhibit page to list page

--- src/Exhibit/pages/ExhibitListPage.jsx
+++ src/Exhibit/pages/ExhibitListPage.jsx
@@ -4,14 +4,19 @@ import PageHeader from "../../general/PageHeader";
 import ExhibitFeedback from "../components/ExhibitFeedback";
 import useExhibitData from "../hooks/useExhibitData";
 import AddNewExhibitButton from "../components/AddExhibitButton";
+import { useNavigate } from "react-router-dom";
 
 export default function ExhibitListPage() {
     const { exhibits, isLoading, error, fetchExhibits } = useExhibitData();
+    const navigate = useNavigate(); // Hook for navigation
 
     useEffect(() => {
         fetchExhibits();
     }, [fetchExhibits]);
 
+    const handleAddExhibit = () => {
+        navigate("/add-exhibit"); // Navigate to AddExhibitPage
+    };
 
     // Define handleDelete and handleEditExhibit here if you want to keep them in this component
     const handleDelete = useCallback((id) => {
@@ -38,7 +43,8 @@ export default function ExhibitListPage() {
                 handleDelete={handleDelete}
                 handleEditExhibit={handleEditExhibit}
             />
-            <AddNewExhibitButton />
+            {/* Add New Exhibit Button */}
+            < AddNewExhibitButton onAddExhibit={handleAddExhibit} />
         </Container>
     );
 }
