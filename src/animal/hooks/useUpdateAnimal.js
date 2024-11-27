e963301ea5dcb711d7873b46ed61356b4ea8136e
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 16:56:23 +0200

    add snackbar messages + add use callback instead of use effect in update animal hook. + add normalize animal as dependency

--- src/animal/hooks/useUpdateAnimal.js
+++ src/animal/hooks/useUpdateAnimal.js
@@ -1,25 +1,31 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { updateAnimal } from "../../services/AnimalServiceApi";
+import { useSnack } from "../../providers/SnackbarProvider";
+import normalizeAnimal from "../helpers/normalizeAnimal";
 
 const useUpdateAnimal = () => {
+    const [animal, setAnimal] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+    const setSnack = useSnack();
 
-    const update = async (id, animal) => {
+    const handleUpdateAnimal = useCallback(async (id, animalFromClient) => {
         setLoading(true);
         setError(null);
-
         try {
-            const data = await updateAnimal(id, animal);
-            return data;
+            const normalizedAnimal = normalizeAnimal(animalFromClient)
+            const updatedAnimal = await updateAnimal(id, normalizedAnimal);
+            setAnimal(updatedAnimal);
+            setSnack('success', `Animal ${id} updated successfully!`);
         } catch (err) {
             setError(err.message);
+            setSnack('error', `Failed to update animal ${id}`);
         } finally {
             setLoading(false);
         }
-    };
+    }, [setSnack, normalizeAnimal]);
 
-    return { update, loading, error };
+    return { animal, loading, error, handleUpdateAnimal };
 };
 
 export default useUpdateAnimal;
