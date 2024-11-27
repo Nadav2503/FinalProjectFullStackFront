094c6cc933b0cfb80270b2609acf7d7b6d431e79
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 18:03:08 +0200

    pass the props

--- src/animal/components/AnimalFeedback.jsx
+++ src/animal/components/AnimalFeedback.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Animals from "./Animals";
 
-export default function AnimalFeedback() {
+export default function AnimalFeedback({ animals, handleDelete, handleEditAnimal, handleFavoriteToggle }) {
 
     return (
         <Animals
-
+            animals={animals}
+            handleDelete={handleDelete}
+            handleEditAnimal={handleEditAnimal}
+            handleFavoriteToggle={handleFavoriteToggle}
         />
     );
 }
