383592efdca369c889f7df49686cfc9e798b26a6
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 16:11:51 +0200

    add use states

--- src/animal/hooks/useGetAnimalsByExhibit.js
+++ src/animal/hooks/useGetAnimalsByExhibit.js
@@ -1,3 +1,13 @@
-const useGetAnimalsByExhibit = (exhibitId) => { };
+import { useState, useEffect } from "react";
+
+const useGetAnimalsByExhibit = (exhibitId) => {
+    const [animals, setAnimals] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+
+
+    return { loading, error };
+};
 
 export default useGetAnimalsByExhibit;
