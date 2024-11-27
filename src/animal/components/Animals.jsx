488cfea2e1d45cb89ca92a54424296d6879b31f4
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 17:59:32 +0200

    pass the function props to animalCArd

--- src/animal/components/Animals.jsx
+++ src/animal/components/Animals.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import AnimalCard from "./card/AnimalCard";
 import { Container } from "@mui/material";
 
-export default function Animals() {
+export default function Animals({ handleDelete, handleEditAnimal, handleFavoriteToggle }) {
     return (
         <Container sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center", gap: 1 }}>
             {
 
                 <AnimalCard
 
+                    handleDelete={handleDelete}
+                    handleEditAnimal={handleEditAnimal}
+                    handleFavoriteToggle={handleFavoriteToggle}
                 />
 
             }
