00aa976b864eb9a6c1c210415ef137ef05cacc8f
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 16:28:56 +0200

    create use effect function to delete animal

--- src/animal/hooks/useDeleteAnimal.js
+++ src/animal/hooks/useDeleteAnimal.js
@@ -1,11 +1,26 @@
 import { useState } from "react";
+import { deleteAnimal } from "../../services/AnimalServiceApi";
 
 
 const useDeleteAnimal = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
+    const remove = async (id) => {
+        setLoading(true);
+        setError(null);
 
+        try {
+            const data = await deleteAnimal(id);
+            return data;
+        } catch (err) {
+            setError(err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return { remove, loading, error };
 };
 
 export default useDeleteAnimal;
