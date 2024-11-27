19f8d3685bd3a62b7fb94f8b65fc0e1b5bc77378
Nadav2503 <nadavivon.re@gmail.com>
2024-11-27 16:23:37 +0200

    add use states

--- src/animal/hooks/useUpdateAnimal.js
+++ src/animal/hooks/useUpdateAnimal.js
@@ -1,7 +1,12 @@
-
+import { useState } from "react";
+import { updateAnimal } from "../../services/AnimalServiceApi";
 
 const useUpdateAnimal = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
+
+    return { loading, error };
 };
 
 export default useUpdateAnimal;
