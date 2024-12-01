b2be27629ae329a4cadff0999493e0521a2478f0
Nadav2503 <nadavivon.re@gmail.com>
2024-12-01 16:10:59 +0200

    add animal form to add animal form

--- src/animal/pages/AddAnimalPage.jsx
+++ src/animal/pages/AddAnimalPage.jsx
@@ -1,7 +1,27 @@
+import { Box, Container } from '@mui/material'
 import React from 'react'
+import AnimalForm from '../components/AnimalForm'
 
 export default function AddAnimalPage() {
     return (
-        <div>AddAnimalPage</div>
+        <Container>
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <AnimalForm
+                    title="Add New Animal"
+                    submitLabel="Create Animal"
+                    onSubmit={onSubmit}
+                    validateForm={validateForm}
+                    errors={errors}
+                    data={data}
+                    onInputChange={handleChange}
+                />
+            </Box>
+        </Container>
     )
 }
