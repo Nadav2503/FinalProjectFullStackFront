85a027efe3ab6b4a9bc7bd478dabd302a830a52e
Nadav2503 <nadavivon.re@gmail.com>
2024-12-01 17:01:50 +0200

    add animal edit form to the edit animal page

--- src/animal/pages/EditAnimalPage.jsx
+++ src/animal/pages/EditAnimalPage.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import AnimalEditForm from '../components/AnimalEditForm'
+import { Box, Container } from '@mui/material'
 
 export default function EditAnimalPage() {
     return (
-        <div>EditAnimalPage</div>
+        <Container>
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <AnimalEditForm
+                    title="Edit Animal"
+                    submitLabel="Update Animal"
+                    onSubmit={onSubmit}
+                    validateForm={validateForm}
+                    errors={errors}
+                    data={data}
+                    onInputChange={handleChange}
+                />
+            </Box>
+        </Container>
     )
 }
