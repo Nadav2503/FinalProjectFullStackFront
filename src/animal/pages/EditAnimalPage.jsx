8927858e65489be10725fbd685e98b9ae3bcb68f
Nadav2503 <nadavivon.re@gmail.com>
2024-12-01 17:02:48 +0200

    add the states and hooks

--- src/animal/pages/EditAnimalPage.jsx
+++ src/animal/pages/EditAnimalPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import AnimalEditForm from '../components/AnimalEditForm'
 import { Box, Container } from '@mui/material'
+import { useNavigate, useParams } from 'react-router-dom';
+import useGetAnimalById from '../hooks/useGetAnimalById';
+import useUpdateAnimal from '../hooks/useUpdateAnimal';
 
 export default function EditAnimalPage() {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const { animal, fetchAnimalById } = useGetAnimalById();
+    const { handleUpdateAnimal } = useUpdateAnimal();
+
     return (
         <Container>
             <Box
