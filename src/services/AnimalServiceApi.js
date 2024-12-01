158dc4d192c59a5b4d131f655851aaf540808397
Nadav2503 <nadavivon.re@gmail.com>
2024-12-01 18:43:53 +0200

    remove get all animals function

--- src/services/AnimalServiceApi.js
+++ src/services/AnimalServiceApi.js
@@ -3,16 +3,6 @@ import axios from "axios";
 // Set the base URL for your API
 const API_URL = "http://localhost:8181/Zoo/animals";
 
-// Get all animals
-export const getAnimals = async () => {
-    try {
-        const { data } = await axios.get(API_URL);
-        return data;
-    } catch (error) {
-        throw new Error(error.message);
-    }
-};
-
 // Get animals by exhibit ID 
 export const getAnimalsByExhibit = async (exhibitId) => {
     try {
