59f4fe80ade3ada32be4b2ec332760f06139bad1
Nadav2503 <nadavivon.re@gmail.com>
2024-12-02 18:25:02 +0200

    add handle submit function to the page

--- src/visitor/pages/SignupPage.jsx
+++ src/visitor/pages/SignupPage.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useCallback } from "react";
 import SignupForm from "../components/SignupForm";
 import useForm from "../../form/useForm";
 import initializeSignup from "../helpers/initialize/initializeSignup";
 import signupSchema from "../model/signupSchema";
 import { Box, Container } from "@mui/material";
-
+import { useNavigate } from "react-router-dom";
+import ROUTES from "../../routers/routerModel";
 import useSignupVisitor from "../hooks/useSignupVisitor";
 
 const SignupPage = () => {
     const { handleSignup } = useSignupVisitor();
+    const navigate = useNavigate();
 
+    const handleFormSubmit = useCallback(
+        async (formData) => {
 
+            try {
+                await handleSignup(formData); // Call signup hook which now also logs in the user
+                navigate(ROUTES.ROOT); // Navigate to the home page after successful signup & login
+            } catch (error) {
+                console.error("Signup failed:", error);
+            }
+        },
+        [handleSignup, navigate]
+    );
 
     const {
         data,
