af5161a6ec399e26ea47d457549c488bacaa54c6
Nadav2503 <nadavivon.re@gmail.com>
2024-12-02 09:06:45 +0200

    add save token in local storage function

--- src/services/LocalStorageService.js
+++ src/services/LocalStorageService.js
@@ -1,4 +1,8 @@
 import { jwtDecode } from 'jwt-decode';
 
-const TOKEN = 'myToken';
+const TOKEN = 'myToken'; // The key used to store the token in localStorage
 
+// Store the token in localStorage
+export const setTokenInLocalStorage = (jwtToken) => {
+    localStorage.setItem(TOKEN, jwtToken);
+}
