a11b36133a2e1dc9aba363f0b4a36928553e7398
Nadav2503 <nadavivon.re@gmail.com>
2024-12-02 11:25:05 +0200

    add use effect to enable delete

--- src/visitor/hooks/useDeleteVisitor.js
+++ src/visitor/hooks/useDeleteVisitor.js
@@ -1,16 +1,31 @@
-import { useState } from 'react';
-
+import { useCallback, useState } from 'react';
+import { deleteVisitor } from '../../services/VisitorServiceApi';
 import { useSnack } from '../../providers/SnackbarProvider';
-
+import { getToken } from '../../services/LocalStorageService';
 
 const useDeleteVisitor = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const setSnack = useSnack();
 
+    const handleDeleteVisitor = useCallback(async (id) => {
+        setLoading(true);
+        setError(null);
 
+        const token = getToken(); // Get the token from localStorage
 
-    return { loading, error };
+        try {
+            await deleteVisitor(id, token); // Pass the token to the delete API
+            setSnack('success', `Visitor ${id} deleted successfully!`);
+        } catch (err) {
+            setError(err.message);
+            setSnack('error', `Failed to delete visitor ${id}`);
+        } finally {
+            setLoading(false);
+        }
+    }, [setSnack]);
+
+    return { handleDeleteVisitor, loading, error };
 };
 
 export default useDeleteVisitor;
