197b577768eca7565a021b441f6ea4854a97ea9c
Nadav2503 <nadavivon.re@gmail.com>
2024-12-02 12:33:59 +0200

    add handle login function and use form hook

--- src/visitor/pages/LoginPage.jsx
+++ src/visitor/pages/LoginPage.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import LoginForm from "../components/LoginForm";
+import useLoginVisitor from "../hooks/useLoginVisitor";
+import useForm from "../../form/useForm";
+import initializeLogin from "../helpers/initialize/initializeLogin";
+import loginSchema from "../model/loginSchema";
 import { Box, Container } from "@mui/material";
 
 const LoginPage = () => {
+    const { handleLogin } = useLoginVisitor();
+
+    const {
+        data,
+        errors,
+        handleChange,
+        validateForm,
+        onSubmit
+    } = useForm(initializeLogin, loginSchema, handleLogin);
 
     return (
         <Container>
