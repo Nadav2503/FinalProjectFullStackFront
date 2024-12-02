3cae5e51f09653eeabaf7c71e8a997ff3584302a
Nadav2503 <nadavivon.re@gmail.com>
2024-12-02 09:40:00 +0200

    create initial signup

---
+++ src/visitor/helpers/initializeSignup.js
@@ -0,0 +1,14 @@
+const initializeSignup = {
+    username: "",
+    firstName: "",
+    middleName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    membershipTier: "Tier 1 - Explorer",
+    phone: "",
+    imageUrl: "",
+    imageAlt: "",
+};
+
+export default initializeSignup;
