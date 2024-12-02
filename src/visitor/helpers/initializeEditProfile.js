82db03206811bdd2f562fe78f60535697108b1bb
Nadav2503 <nadavivon.re@gmail.com>
2024-12-02 09:40:41 +0200

    create initial edit profile

---
+++ src/visitor/helpers/initializeEditProfile.js
@@ -0,0 +1,10 @@
+const initializeEditProfile = {
+    firstName: "",
+    middleName: "",
+    lastName: "",
+    phone: "",
+    imageUrl: "",
+    imageAlt: "",
+};
+
+export default initializeEditProfile;
