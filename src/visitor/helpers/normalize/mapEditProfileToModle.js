eb658e8f2c48ceb52d0b9d2db420d46cc4c449f9
Nadav2503 <nadavivon.re@gmail.com>
2024-12-02 18:14:09 +0200

    correct the names of properties in mapEditProfileToModel

--- src/visitor/helpers/normalize/mapEditProfileToModle.js
+++ src/visitor/helpers/normalize/mapEditProfileToModle.js
@@ -1,8 +1,8 @@
 const mapEditProfileToModel = (profileData) => {
     return {
-        firstName: profileData.firstName,
-        middleName: profileData.middleName || "", // Default to empty string if middle name is not provided
-        lastName: profileData.lastName,
+        first: profileData.first,
+        middle: profileData.middle || "", // Default to empty string if middle name is not provided
+        last: profileData.last,
         phone: profileData.phone || "", // Default to empty string if phone is not provided
         imageUrl: profileData.image?.url || "", // Default to empty string if no image URL is provided
         imageAlt: profileData.image?.alt || "", // Default to empty string if no image alt text is provided
