9e018c4a141fb658d25502bbbcec4f92ddfb9959
Nadav2503 <nadavivon.re@gmail.com>
2024-12-02 09:20:55 +0200

    add use effect to connect to local storage

--- src/providers/VisitorProvider.jsx
+++ src/providers/VisitorProvider.jsx
@@ -1,13 +1,18 @@
-import React, { createContext, useState } from 'react';
-import { getToken } from '../services/LocalStorageService';
-
-
-const VisitorContext = createContext();
+import React, { createContext, useState, useEffect } from 'react';
+import { getToken, getUser } from '../services/LocalStorageService';
 
 export default function VisitorProvider({ children }) {
     const [visitor, setVisitor] = useState(null);
     const [token, setToken] = useState(getToken());
 
+    useEffect(() => {
+        if (token) {
+            const userFromLocalStorage = getUser();
+            setVisitor(userFromLocalStorage); // Set the user from the token
+        } else {
+            setVisitor(null); // If no token, set visitor to null
+        }
+    }, [token]);
 
     return (
         <></>
