2ccab6e53c9160cbc09bc806b715c651c494d6bc
Nadav2503 <nadavivon.re@gmail.com>
2024-12-02 14:20:11 +0200

    add set visitor and set token to the function

--- src/visitor/hooks/useLogout.js
+++ src/visitor/hooks/useLogout.js
@@ -1,13 +1,18 @@
-import { useCallback, useState } from 'react';
+import { useCallback } from 'react';
 import { removeToken } from '../../services/LocalStorageService';
 import { useSnack } from '../../providers/SnackbarProvider';
+import { useCurrentVisitor } from '../../providers/VisitorProvider';
 
 const useLogout = () => {
-    const [loading, setLoading] = useState(false);
     const setSnack = useSnack();
+    const { setToken, setVisitor } = useCurrentVisitor();
+
     const handleLogout = useCallback(() => {
         removeToken();  // Remove the token from localStorage
-    }, []);
+        setToken(null);  // Reset the token in the global state
+        setVisitor(null); // Reset the user state in the global state
+        setSnack('success', 'Logged out successfully');
+    }, [setSnack, setToken, setVisitor]);
 
     return { handleLogout };
 };
