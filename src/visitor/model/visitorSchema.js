61324fb1f2a38c5e624bd2df5d565369f2068ec9
Nadav2503 <nadavivon.re@gmail.com>
2024-12-02 18:08:30 +0200

    correct the properties in VisitorSchema

--- src/visitor/model/visitorSchema.js
+++ src/visitor/model/visitorSchema.js
@@ -2,9 +2,9 @@ import Joi from "joi";
 
 // Profile schema for visitor details
 const profileSchema = {
-    firstName: Joi.string().min(1).max(256).required(), // First name
-    middleName: Joi.string().min(0).max(256).optional(), // Middle name
-    lastName: Joi.string().min(1).max(256).required(), // Last name
+    first: Joi.string().min(1).max(256).required(), // First name
+    middle: Joi.string().min(0).max(256).optional(), // Middle name
+    last: Joi.string().min(1).max(256).required(), // Last name
     phone: Joi.string()
         .pattern(/^(?:\+972-?5\d{2}-?\d{4}|(?:\+972|0)?50-?\d{7})$/)
         .optional(), // Phone number
