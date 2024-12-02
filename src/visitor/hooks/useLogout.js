2970de0dd5e71866b901bbc720a15f1ec568f7c2
Nadav2503 <nadavivon.re@gmail.com>
2024-12-02 13:56:15 +0200

    add useStates to the logout hook

--- src/visitor/hooks/useLogout.js
+++ src/visitor/hooks/useLogout.js
@@ -1,8 +1,10 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { removeToken } from '../../services/LocalStorageService';
+import { useSnack } from '../../providers/SnackbarProvider';
 
 const useLogout = () => {
-
+    const [loading, setLoading] = useState(false);
+    const setSnack = useSnack();
     const handleLogout = useCallback(() => {
         removeToken();  // Remove the token from localStorage
     }, []);
