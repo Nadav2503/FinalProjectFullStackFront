1490450bd2e3f507cec0a74ae31bce9ef8c053c0
Nadav2503 <nadavivon.re@gmail.com>
2024-12-02 11:35:48 +0200

    add use effect to enable adding/removing of animal to/from the arry

--- src/visitor/hooks/useLikeAnimal.js
+++ src/visitor/hooks/useLikeAnimal.js
@@ -1,16 +1,34 @@
-import { useState } from 'react';
-
+import { useState, useCallback } from 'react';
+import { likeAnimal } from '../../services/VisitorServiceApi';
 import { useSnack } from '../../providers/SnackbarProvider';
-
+import { getToken } from '../../services/LocalStorageService';
 
 const useLikeAnimal = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const setSnack = useSnack();
 
+    const handleLikeAnimal = useCallback(async (visitorId, animalId) => {
+        setLoading(true);
+        setError(null);
 
+        const token = getToken(); // Get token from localStorage
 
-    return { loading, error };
+        try {
+            // Call API to toggle the like status for the animal
+            await likeAnimal(visitorId, animalId, token); // Pass the token for authentication
+
+            // Show success notification after the API call
+            setSnack('success', `Animal ${animalId} like status updated!`);
+        } catch (err) {
+            setError(err.message);
+            setSnack('error', `Failed to like animal ${animalId}`);
+        } finally {
+            setLoading(false);
+        }
+    }, [setSnack]);
+
+    return { handleLikeAnimal, loading, error };
 };
 
 export default useLikeAnimal;
