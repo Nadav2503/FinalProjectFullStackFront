93271f4e904b3b962be4ef74d56fbb60e331d83c
Nadav2503 <nadavivon.re@gmail.com>
2024-12-02 10:33:00 +0200

    add use effecr to enable login

--- src/visitor/hooks/useLoginVisitor.js
+++ src/visitor/hooks/useLoginVisitor.js
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
+import { loginVisitor } from '../../services/VisitorServiceApi';
+import { setTokenInLocalStorage } from '../../services/LocalStorageService';
 import { useSnack } from '../../providers/SnackbarProvider';
 import { useCurrentVisitor } from '../../providers/VisitorProvider';
 
@@ -8,11 +10,34 @@ export default function useLoginVisitor() {
     const setSnack = useSnack();
     const { setVisitor, setToken } = useCurrentVisitor();
 
+    const handleLogin = useCallback(async (userLogin) => {
+        setIsLoading(true);
+        setError(null);  // Clear previous error if any
 
+        try {
+            // Try to login the user and retrieve the visitor data and token
+            const { token, visitorData } = await loginVisitor(userLogin);  // Adjust API call response accordingly
+
+            // Store token in local storage and set the token in the state
+            setTokenInLocalStorage(token);
+            setToken(token);
+
+            // Set the visitor data in context/state
+            setVisitor(visitorData);
+
+            // Successfully logged in, show success message
+            setSnack('success', 'Login successful!');
+        } catch (err) {
+            setError(err.message);  // Set error if fetching fails
+            setSnack('error', "Failed to Login");  // Show error message
+        } finally {
+            setIsLoading(false);
+        }
+    }, [setSnack, setToken, setVisitor]);
 
     return {
         isLoading,
         error,
-
+        handleLogin,
     };
 }
