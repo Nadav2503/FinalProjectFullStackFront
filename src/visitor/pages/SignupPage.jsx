cad69ca0d3b42f90400c50535b850852cb917f5b
Nadav2503 <nadavivon.re@gmail.com>
2024-12-02 18:24:35 +0200

    add use form hook to the page

--- src/visitor/pages/SignupPage.jsx
+++ src/visitor/pages/SignupPage.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import SignupForm from "../components/SignupForm";
-
+import useForm from "../../form/useForm";
+import initializeSignup from "../helpers/initialize/initializeSignup";
+import signupSchema from "../model/signupSchema";
 import { Box, Container } from "@mui/material";
 
+import useSignupVisitor from "../hooks/useSignupVisitor";
 
 const SignupPage = () => {
+    const { handleSignup } = useSignupVisitor();
 
+
+
+    const {
+        data,
+        errors,
+        handleChange,
+        validateForm,
+        onSubmit,
+    } = useForm(initializeSignup, signupSchema, handleFormSubmit);
 
     return (
         <Container>
