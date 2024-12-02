2437a13d0a73d917a429ae1ea73acdc0f9b93211
Nadav2503 <nadavivon.re@gmail.com>
2024-12-02 09:02:08 +0200

    add get all visitors endpoints

--- src/services/VisitorServiceApi.js
+++ src/services/VisitorServiceApi.js
@@ -2,3 +2,14 @@ import axios from "axios";
 
 // Set the base URL for your API
 const API_URL = "http://localhost:8181/Zoo/visitors";
+
+// Get all visitors (admin only)
+export const getAllVisitors = async () => {
+    try {
+        const { data } = await axios.get(API_URL);
+        return data;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
+
