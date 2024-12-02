76e5fcd1d892f00252d5b3b2c1a733b15d8f0d35
Nadav2503 <nadavivon.re@gmail.com>
2024-12-02 11:55:31 +0200

    add the Form component and the props to signup form

---
+++ src/visitor/components/SignupForm.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Form from "../../form/Form";
+
+
+const SignupForm = ({
+    onSubmit,
+    onInputChange,
+    errors = {},
+    data = {},
+    title,
+    submitLabel,
+    validateForm,
+    isSubmitting,
+}) => {
+    return (
+        <Form
+
+        >
+
+        </Form>
+    );
+};
+
+export default SignupForm;
