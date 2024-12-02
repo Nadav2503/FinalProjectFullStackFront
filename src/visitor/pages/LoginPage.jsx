089c2a66284c4beacd2f4891e6f0093ece940af1
Nadav2503 <nadavivon.re@gmail.com>
2024-12-02 12:38:27 +0200

    add handle submit function

--- src/visitor/pages/LoginPage.jsx
+++ src/visitor/pages/LoginPage.jsx
@@ -5,9 +5,25 @@ import useForm from "../../form/useForm";
 import initializeLogin from "../helpers/initialize/initializeLogin";
 import loginSchema from "../model/loginSchema";
 import { Box, Container } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import ROUTES from "../../routers/routerModel";
 
 const LoginPage = () => {
     const { handleLogin } = useLoginVisitor();
+    const navigate = useNavigate();
+
+
+    const handleSubmit = useCallback(
+        async (formData) => {
+            try {
+                await handleLogin(formData);
+                navigate(ROUTES.HOME);
+            } catch (error) {
+                console.error("Login failed:", error);
+            }
+        },
+        [handleLogin, navigate]
+    );
 
     const {
         data,
@@ -15,7 +31,7 @@ const LoginPage = () => {
         handleChange,
         validateForm,
         onSubmit
-    } = useForm(initializeLogin, loginSchema, handleLogin);
+    } = useForm(initializeLogin, loginSchema, handleSubmit);
 
     return (
         <Container>
