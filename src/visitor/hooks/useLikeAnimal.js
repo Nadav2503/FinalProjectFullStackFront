4827a7d657f856ba6c0d2bd819bb6a1298e4f81c
Nadav2503 <nadavivon.re@gmail.com>
2024-12-04 18:47:57 +0200

    improve the handle like animal functiomn. 1)split the funtion to two diffrent cases: is about to be favorite, and already been favorite. 2)add visitor through context so the function would know when the user login , that when he enter the exhibit to shown the animal that are favorite with red icon

--- src/visitor/hooks/useLikeAnimal.js
+++ src/visitor/hooks/useLikeAnimal.js
@@ -1,21 +1,46 @@
-import { useState, useCallback } from 'react';
-import { likeAnimal } from '../../services/VisitorServiceApi';
-import { useSnack } from '../../providers/SnackbarProvider';
+import { useState, useCallback } from "react";
+import { likeAnimal } from "../../services/VisitorServiceApi";
+import { useSnack } from "../../providers/SnackbarProvider";
+import { useCurrentVisitor } from "../../providers/VisitorProvider";
 
 const useLikeAnimal = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+    const { visitor, setVisitor } = useCurrentVisitor();
     const setSnack = useSnack();
 
-    const handleLikeAnimal = useCallback(async (visitorId, animalId) => {
+    const handleLikeAnimal = useCallback(async (animalId) => {
+        if (!visitor) {
+            console.error("Visitor data is not available");
+            return;
+        }
+
         setLoading(true);
         setError(null);
 
         try {
-            // Call API to toggle the like status for the animal
-            await likeAnimal(visitorId, animalId); // Pass the token for authentication
+            const preferredAnimals = visitor?.preferredAnimals || [];
 
-            // Show success notification after the API call
+            if (preferredAnimals.includes(animalId)) {
+                await likeAnimal(visitor._id, animalId);
+                setVisitor((prevVisitor) => {
+                    const updatedVisitor = {
+                        ...prevVisitor,
+                        preferredAnimals: prevVisitor.preferredAnimals.filter(id => id !== animalId),
+                    };
+                    return updatedVisitor;
+                });
+            } else {
+                await likeAnimal(visitor._id, animalId);
+                setVisitor((prevVisitor) => {
+                    const updatedVisitor = {
+                        ...prevVisitor,
+                        preferredAnimals: [...(prevVisitor.preferredAnimals || []), animalId],
+                    };
+                    return updatedVisitor;
+                });
+            }
+
             setSnack('success', `Animal ${animalId} like status updated!`);
         } catch (err) {
             setError(err.message);
@@ -23,7 +48,7 @@ const useLikeAnimal = () => {
         } finally {
             setLoading(false);
         }
-    }, [setSnack]);
+    }, [setSnack, visitor, setVisitor]);
 
     return { handleLikeAnimal, loading, error };
 };
