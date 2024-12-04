83114df0d3c1b2c044d7a676d84ccb331ae76866
Nadav2503 <nadavivon.re@gmail.com>
2024-12-04 18:41:33 +0200

    add is liked as props to animal card through animals

--- src/animal/components/Animals.jsx
+++ src/animal/components/Animals.jsx
@@ -4,24 +4,25 @@ import { Container } from "@mui/material";
 
 export default function Animals({ animals, handleDelete, handleEditAnimal, handleFavoriteToggle }) {
     return (
-        <Container sx={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'center',
-            gap: 2, // Increase gap for better spacing
-            alignItems: 'stretch', // Ensures cards align in height
-        }}>
-            {
-                animals.map((animal) => (
-                    <AnimalCard
-                        animal={animal}
-                        key={animal._id}
-                        handleDelete={handleDelete}
-                        handleEditAnimal={handleEditAnimal}
-                        handleFavoriteToggle={handleFavoriteToggle}
-                    />
-                ))
-            }
+        <Container
+            sx={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                justifyContent: 'center',
+                gap: 2,
+                alignItems: 'stretch',
+            }}
+        >
+            {animals.map((animal) => (
+                <AnimalCard
+                    animal={animal}
+                    key={animal._id}
+                    handleDelete={handleDelete}
+                    handleEditAnimal={handleEditAnimal}
+                    handleFavoriteToggle={handleFavoriteToggle}
+                    isLiked={animal.isLiked}
+                />
+            ))}
         </Container>
     );
 }
