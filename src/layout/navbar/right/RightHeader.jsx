e99ae3c9e88b7e5619e471d90a93ffcbb9f65a41
Nadav2503 <nadavivon.re@gmail.com>
2024-12-04 18:12:21 +0200

    create use effect to get the username

--- src/layout/navbar/right/RightHeader.jsx
+++ src/layout/navbar/right/RightHeader.jsx
@@ -1,15 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box } from '@mui/material';
 import AvatarProfileImage from './Avatar';
 import AvatarMenu from './AvatarMenuAnchor';
 import useAnchor from '../useAnchor';
 import { useCurrentVisitor } from '../../../providers/VisitorProvider';
-
+import { getVisitorById } from '../../../services/VisitorServiceApi';
 
 export default function RightHeader() {
     const { anchorEl, handleAnchorClick, handleAnchorClose } = useAnchor();
     const { visitor } = useCurrentVisitor(); // Access visitor context
     const [username, setUsername] = useState(null);
+    useEffect(() => {
+        const fetchUsername = async () => {
+            if (visitor && visitor._id) { // Check visitor object
+                try {
+                    const visitorData = await getVisitorById(visitor._id); // Fetch visitor details
+                    setUsername(visitorData.username); // Update username state
+                } catch (error) {
+                    console.error('Error fetching visitor details:', error);
+                }
+            }
+        };
+
+        if (visitor && visitor._id) {
+            fetchUsername();
+        }
+    }, [visitor]); // Only rerun if visitor changes
 
     return (
         <Box
@@ -23,6 +39,7 @@ export default function RightHeader() {
         >
             {/* Avatar button */}
             <AvatarProfileImage onClick={handleAnchorClick} />
+
 
             {/* Dropdown menu */}
             <AvatarMenu anchorEl={anchorEl} onClose={handleAnchorClose} />
