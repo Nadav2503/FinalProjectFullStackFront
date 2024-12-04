96089c146e7885ab8d733bfbc674a3f4a7f83f6c
Nadav2503 <nadavivon.re@gmail.com>
2024-12-04 18:43:48 +0200

    create handle favorite click function and add is liked as props

--- src/animal/components/card/AnimalCard.jsx
+++ src/animal/components/card/AnimalCard.jsx
@@ -7,23 +7,30 @@ import AnimalActionBar from './AnimalActionBar';
 import ROUTES from '../../../routers/routerModel';
 import { useNavigate } from 'react-router-dom';
 
-export default function AnimalCard({ animal, handleDelete, handleEditAnimal, handleFavoriteToggle }) {
+export default function AnimalCard({ animal, handleDelete, handleEditAnimal, handleFavoriteToggle, isLiked }) {
     const navigate = useNavigate();
+
+    const handleFavoriteClick = () => {
+        handleFavoriteToggle(animal._id);
+    };
 
     const handleCardClick = () => {
         navigate(`${ROUTES.ANIMAL_INFO}/${animal._id}`);
     };
+
     return (
         <Card>
             <AnimalHeader title={animal.name} image={animal.image} />
             <CardActionArea onClick={handleCardClick}>
-                <AnimalBody
-                    type={animal.type}
-                    gender={animal.gender}
-                    age={animal.age}
-                />
+                <AnimalBody type={animal.type} gender={animal.gender} age={animal.age} />
             </CardActionArea>
-            <AnimalActionBar animalId={animal._id} handleDelete={handleDelete} handleEditAnimal={handleEditAnimal} handleFavoriteToggle={handleFavoriteToggle} />
+            <AnimalActionBar
+                animalId={animal._id}
+                handleDelete={handleDelete}
+                handleEditAnimal={handleEditAnimal}
+                handleFavoriteToggle={handleFavoriteClick}
+
+            />
         </Card>
     );
 }
