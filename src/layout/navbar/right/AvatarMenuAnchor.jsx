3f8c6de52598004caf948582da594be74888cecc
Nadav2503 <nadavivon.re@gmail.com>
2024-12-04 19:26:37 +0200

    connect edit profile page to the navbar

--- src/layout/navbar/right/AvatarMenuAnchor.jsx
+++ src/layout/navbar/right/AvatarMenuAnchor.jsx
@@ -29,7 +29,7 @@ export default function AvatarMenu({ anchorEl, onClose }) {
             >
                 {/* Navigation items with close on click */}
                 <NavBarItem label="Profile" variant="vertical" onClick={onClose} />
-                <NavBarItem label="Edit Profile" variant="vertical" onClick={onClose} />
+                <NavBarItem label="Edit Profile" variant="vertical" to={ROUTES.EDIT_PROFILE} onClick={onClose} />
                 <NavBarItem label="Login" variant="vertical" onClick={onClose} to={ROUTES.LOGIN} />
                 <NavBarItem label="Signup" variant="vertical" onClick={onClose} to={ROUTES.SIGNUP} />
                 <NavBarItem label="Logout" variant="vertical" to={ROUTES.ROOT} onClick={() => { handleLogout(); onClose(); }} />
