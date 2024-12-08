636ba4de5497fe97e75dc8f4ccbcfb17aeeb6763
Nadav2503 <nadavivon.re@gmail.com>
2024-12-08 18:45:58 +0200

    add conditional renderings

--- src/visitor/pages/ProfilePage.jsx
+++ src/visitor/pages/ProfilePage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { getUser } from "../../services/LocalStorageService";
-
+import Loader from "../../general/Loader";
+import Error from "../../general/Error";
 export default function ProfilePage() {
     const user = getUser();  // Decode the token and get the user data (id, etc.)
     const { visitor, loading, error, fetchVisitorById } = useGetVisitorById();
@@ -9,6 +10,12 @@ export default function ProfilePage() {
             fetchVisitorById(user._id);
         }
     }, [user, visitor, fetchVisitorById]);
+    if (loading) return <Loader />;
+    if (error) {
+        const errorMessage = typeof error === "string" ? error : error.message || "An unknown error occurred.";
+        return <Error errorMessage={errorMessage} />;
+    }
+    if (!visitor) return <Error errorMessage="Visitor not found." />;
     return (
         <div>ProfilePage</div>
     )
