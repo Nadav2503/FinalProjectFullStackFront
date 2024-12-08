c9c76574a3e4604774a4ff87738f553f2c77e89a
Nadav2503 <nadavivon.re@gmail.com>
2024-12-08 18:52:09 +0200

    add isAdmin status

--- src/visitor/pages/ProfilePage.jsx
+++ src/visitor/pages/ProfilePage.jsx
@@ -48,7 +48,9 @@ export default function ProfilePage() {
                         <Grid item xs={12} sm={6}>
                             <Typography variant="body1"><strong>Membership Tier:</strong> {visitor.membershipTier}</Typography>
                         </Grid>
-
+                        <Grid item xs={12} sm={6}>
+                            <Typography variant="body1"><strong>Admin:</strong> {visitor.isAdmin ? "Yes" : "No"}</Typography>
+                        </Grid>
                     </Grid>
                 </CardContent>
             </Card></Container>
