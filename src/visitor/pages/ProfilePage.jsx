5195bb745803c8a5f3b010d1095c7d1d16987bab
Nadav2503 <nadavivon.re@gmail.com>
2024-12-08 18:45:02 +0200

    add use effect to check if user exist

--- src/visitor/pages/ProfilePage.jsx
+++ src/visitor/pages/ProfilePage.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { getUser } from "../../services/LocalStorageService";
 
 export default function ProfilePage() {
     const user = getUser();  // Decode the token and get the user data (id, etc.)
     const { visitor, loading, error, fetchVisitorById } = useGetVisitorById();
+    useEffect(() => {
+        if (user && !visitor) {
+            fetchVisitorById(user._id);
+        }
+    }, [user, visitor, fetchVisitorById]);
     return (
         <div>ProfilePage</div>
     )
