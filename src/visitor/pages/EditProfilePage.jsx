95205cc4d4df17f90bfb9048baeeb3ae4bda3117
Nadav2503 <nadavivon.re@gmail.com>
2024-12-09 15:52:10 +0200

    add the hooks that i need to get the data and update the profile + add use param and use navigate react hooks

--- src/visitor/pages/EditProfilePage.jsx
+++ src/visitor/pages/EditProfilePage.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { Box, Container } from "@mui/material";
+import useUpdateVisitorProfile from "../hooks/useUpdateVisitorProfile";
 import EditProfileForm from "../components/EditProfileForm";
+import useGetVisitorById from "../hooks/useVisitorDataById";
+
 export default function EditProfilePage() {
+    const { id } = useParams();
+    const { visitor, fetchVisitorById } = useGetVisitorById();
+    const { handleUpdateProfile } = useUpdateVisitorProfile();
+    const navigate = useNavigate();
+
     return (
         <Container>
             <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
