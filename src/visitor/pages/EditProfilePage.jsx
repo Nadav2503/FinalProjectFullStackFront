bd8d65d51ddeb698aa19e12cbfa362b5b26ecbd4
Nadav2503 <nadavivon.re@gmail.com>
2024-12-09 15:49:28 +0200

    add the editProfileForm component to the edit profile page

--- src/visitor/pages/EditProfilePage.jsx
+++ src/visitor/pages/EditProfilePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Box, Container } from "@mui/material";
-
+import EditProfileForm from "../components/EditProfileForm";
 export default function EditProfilePage() {
     return (
         <Container>
             <Box >
+                <EditProfileForm
 
+                />
             </Box>
         </Container>
     )
