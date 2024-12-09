61400c8d1adffec7fb181f74f66648d464b7d1d2
Nadav2503 <nadavivon.re@gmail.com>
2024-12-09 15:48:41 +0200

    replace the div with container and box comnponents

--- src/visitor/pages/EditProfilePage.jsx
+++ src/visitor/pages/EditProfilePage.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Box, Container } from "@mui/material";
 
 export default function EditProfilePage() {
     return (
-        <div>EditProfilePage</div>
+        <Container>
+            <Box >
+
+            </Box>
+        </Container>
     )
 }
