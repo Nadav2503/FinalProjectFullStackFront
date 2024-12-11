baaa5bed7aef0eb97ec40992c4cbdf38ed071d4e
Nadav2503 <nadavivon.re@gmail.com>
2024-12-11 11:59:19 +0200

    add the props to Form component in ReviewForm

--- src/review/components/ReviewForm.jsx
+++ src/review/components/ReviewForm.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Form from '../../form/Form';
 
-
 const ReviewForm = ({
     onSubmit,
     onInputChange,
@@ -13,9 +12,13 @@ const ReviewForm = ({
 }) => {
     return (
         <Form
-
+            onSubmit={onSubmit}
+            errors={errors}
+            styles={{ maxWidth: "800px" }}
+            validateForm={validateForm}
+            title={title}
+            submitLabel={submitLabel}
         >
-
 
         </Form>
     );
