f82f138886ce9f92bc7bc6fd8aaa0170a82ff51e
Nadav2503 <nadavivon.re@gmail.com>
2024-12-11 12:17:00 +0200

    create Reviews component for individual review card represent

--- src/review/components/ReviewFeedback.jsx
+++ src/review/components/ReviewFeedback.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Reviews from "./Reviews";
-import Loader from "../../general/Loader";
-import Error from "../../general/Error";
+
 
 export default function ReviewFeedback({
     isLoading,
@@ -9,9 +7,7 @@ export default function ReviewFeedback({
     error,
 
 }) {
-    if (isLoading) return <Loader />;
-    if (error) return <Error errorMessage={error} />;
-    if (!reviews || reviews.length === 0) return <Error errorMessage="Oops...no reviews to display" />;
+
 
     return <></>;
 }
