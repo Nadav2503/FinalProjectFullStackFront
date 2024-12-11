908ed321d0d1d4bde5e51e1e09eb416658fce799
Nadav2503 <nadavivon.re@gmail.com>
2024-12-11 08:46:44 +0200

    create endpoints for getRevieesForAnimal

--- src/services/ReviewServiceAPi.js
+++ src/services/ReviewServiceAPi.js
@@ -3,3 +3,15 @@ import { getToken } from "./LocalStorageService";
 
 // Set the base URL for the reviews API
 const API_URL = "http://localhost:8181/Zoo/reviews";
+
+// Get all reviews for a specific animal
+export const getReviewsForAnimal = async (animalId) => {
+    try {
+        const { data } = await axios.get(`${API_URL}/animal/${animalId}`, {
+            headers: { "x-auth-token": getToken() },
+        });
+        return data;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
