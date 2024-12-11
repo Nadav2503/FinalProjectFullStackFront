528b28e29a266a2e3c12b3d7e970a061080ea566
Nadav2503 <nadavivon.re@gmail.com>
2024-12-11 12:18:11 +0200

    add container mui components and props

---
+++ src/review/components/Reviews.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReviewCard from "./card/ReviewCard";
+import { Container } from "@mui/material";
+
+export default function Reviews({ reviews, handleDelete, handleEdit, handleLike }) {
+    return (
+        <Container
+
+        >
+
+        </Container>
+    );
+}
