48fc31029a2db752b98ae5764d305c62e6372a11
Nadav2503 <nadavivon.re@gmail.com>
2024-12-11 13:21:00 +0200

    add useStates

--- src/review/pages/AddReviewPage.jsx
+++ src/review/pages/AddReviewPage.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 
+import { useNavigate } from "react-router-dom";
+
+import useCreateReview from "../hooks/useCreateReview";
+
 import { Box, Container } from "@mui/material";
 
 export default function AddReviewPage() {
+    const { handleCreate } = useCreateReview();
+    const navigate = useNavigate();
+
 
 
     return (
