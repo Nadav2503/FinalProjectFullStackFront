89d82b1d72242f576025b9fb2654b21ec16ba07a
Nadav2503 <nadavivon.re@gmail.com>
2024-12-11 11:52:28 +0200

    add props and imports to the rating component

---
+++ src/form/Rating.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Grid, Typography, Rating } from '@mui/material';
+
+const RatingField = ({
+    name,
+    label,
+    data,
+    error,
+    onChange,
+    required = true,
+    ...rest
+}) => {
+    return (
+        
+    );
+};
+
+export default RatingField;
