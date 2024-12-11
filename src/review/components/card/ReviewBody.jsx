00578749bd1e4096738b3bd46083319ad275fa3b
Nadav2503 <nadavivon.re@gmail.com>
2024-12-11 12:25:06 +0200

    add rating mui component to rating in ReviewBody

--- src/review/components/card/ReviewBody.jsx
+++ src/review/components/card/ReviewBody.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import CardBody from '../../../general/card/CardBody';
-import { Typography } from '@mui/material';
+import { Rating, Typography } from '@mui/material';
 
 export default function ReviewBody({ comment, rating, date }) {
     const content = (
         <>
             <Typography variant="body1">{comment}</Typography> {/* Review Comment */}
-            <Typography variant="body2">Rating: {rating}</Typography> {/* Rating */}
+            <Typography variant="body2">
+                Rating: <Rating name="read-only" value={rating} readOnly /> {/* Rating */}
+            </Typography>
             <Typography variant="body2">{date}</Typography> {/* Review Date */}
         </>
     )
