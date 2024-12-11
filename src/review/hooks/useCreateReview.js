68eb965cc9f23757cef15dc17f1ef4a183a63868
Nadav2503 <nadavivon.re@gmail.com>
2024-12-11 16:56:00 +0200

    add the user._id in the normalize in create review

--- src/review/hooks/useCreateReview.js
+++ src/review/hooks/useCreateReview.js
@@ -2,17 +2,18 @@ import { useState, useCallback } from "react";
 import { createReview } from "../../services/ReviewServiceApi";
 import { useSnack } from "../../providers/SnackbarProvider";
 import normalizeReview from "../helpers/normalizeReview";
+import { getUser } from "../../services/LocalStorageService";
 
 const useCreateReview = () => {
     const [review, setReview] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
     const setSnack = useSnack();
-
+    const { user } = getUser();
     const handleCreate = useCallback(async (reviewData) => {
         setLoading(true);
         try {
-            const normalizedReview = normalizeReview(reviewData)
+            const normalizedReview = normalizeReview(reviewData, user._id);
             const data = await createReview(normalizedReview); // API call
             setReview(data)
             setSnack("success", "Review created successfully!");
