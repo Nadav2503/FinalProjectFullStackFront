15b7e15eb3bcdff5c697d616af1ac40025fcffcc
Nadav2503 <nadavivon.re@gmail.com>
2024-12-11 11:34:08 +0200

    create the review body without the cardBody component for now

--- src/review/components/card/ReviewBody.jsx
+++ src/review/components/card/ReviewBody.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Typography } from '@mui/material';
 
-export default function ReviewBody() {
+export default function ReviewBody({ comment, rating, date }) {
     return (
-        <div>ReviewBody</div>
-    )
+        <div>
+            <Typography variant="body1">{comment}</Typography> {/* Review Comment */}
+            <Typography variant="body2">Rating: {rating}</Typography> {/* Rating */}
+            <Typography variant="body2">{date}</Typography> {/* Review Date */}
+        </div>
+    );
 }
