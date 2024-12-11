66cd7ddfd22dc137388495b03faf0e60584cffa4
Nadav2503 <nadavivon.re@gmail.com>
2024-12-11 13:23:40 +0200

    create handlSubmit function

--- src/review/pages/AddReviewPage.jsx
+++ src/review/pages/AddReviewPage.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useCallback } from "react";
 import ReviewForm from "../components/ReviewForm";
-
+import initializeReview from "../helpers/initializeReview";
 import { useNavigate } from "react-router-dom";
-
+import useForm from "../../form/useForm";
 import useCreateReview from "../hooks/useCreateReview";
-
+import reviewSchema from "../model/reviewSchema";
 import { Box, Container } from "@mui/material";
 
 export default function AddReviewPage() {
     const { handleCreate } = useCreateReview();
     const navigate = useNavigate();
+
+    const handleSubmit = useCallback(async (formData) => {
+        try {
+
+            await handleCreate(formData); // Submit the review
+            navigate("/"); // Redirect to the reviews page
+        } catch (error) {
+            console.error("Failed to create review:", error);
+        }
+    }, [handleCreate, navigate]);
 
 
 
