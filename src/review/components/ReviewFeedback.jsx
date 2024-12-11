c1a52aabc5088f2f06c8249498d622b9d57c3124
Nadav2503 <nadavivon.re@gmail.com>
2024-12-11 12:20:53 +0200

    add props for Reviews component in ReviewFeedback.

--- src/review/components/ReviewFeedback.jsx
+++ src/review/components/ReviewFeedback.jsx
@@ -1,13 +1,26 @@
 import React from "react";
-
+import Reviews from "./Reviews";
+import Loader from "../../general/Loader";
+import Error from "../../general/Error";
 
 export default function ReviewFeedback({
     isLoading,
     reviews,
     error,
-
+    handleDelete,
+    handleEdit,
+    handleLike,
 }) {
+    if (isLoading) return <Loader />;
+    if (error) return <Error errorMessage={error} />;
+    if (!reviews || reviews.length === 0) return <Error errorMessage="Oops...no reviews to display" />;
 
-
-    return <></>;
+    return (
+        <Reviews
+            reviews={reviews}
+            handleDelete={handleDelete}
+            handleEdit={handleEdit}
+            handleLike={handleLike}
+        />
+    );
 }
