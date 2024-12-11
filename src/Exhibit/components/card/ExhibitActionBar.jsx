9228adb80efe5faeb1aad74bf2b32404bb034411
Nadav2503 <nadavivon.re@gmail.com>
2024-12-11 16:04:15 +0200

    create action for add review button to exhibitActionBar + naviagte to add review when click on the button

--- src/Exhibit/components/card/ExhibitActionBar.jsx
+++ src/Exhibit/components/card/ExhibitActionBar.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CardActionBar from '../../../general/card/CardActionBar';
+import ROUTES from '../../../routers/routerModel';
+import CommentIcon from '@mui/icons-material/Comment';
+import { useNavigate } from 'react-router-dom';
 
 export default function ExhibitActionBar({ exhibitId, handleDelete, handleEditExhibit }) {
+    const navigate = useNavigate();
     const actions = [
         {
             onClick: () => handleEditExhibit(exhibitId), // Call handleEditExhibit when the edit icon is clicked
@@ -12,6 +16,10 @@ export default function ExhibitActionBar({ exhibitId, handleDelete, handleEditEx
         {
             onClick: () => handleDelete(exhibitId), // Call handleDelete when the delete icon is clicked
             icon: <DeleteIcon />,
+        },
+        {
+            onClick: () => navigate(`${ROUTES.ADD_REVIEW}?exhibitId=${exhibitId}`),
+            icon: <CommentIcon />,
         },
     ];
 
