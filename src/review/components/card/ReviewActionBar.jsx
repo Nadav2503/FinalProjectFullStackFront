c1290ddf233e81f276cb617d4966e0062ebbd403
Nadav2503 <nadavivon.re@gmail.com>
2024-12-11 11:38:08 +0200

    create review action bar with icon to delete, edit and like review

--- src/review/components/card/ReviewActionBar.jsx
+++ src/review/components/card/ReviewActionBar.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from 'react';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import CardActionBar from '../../../general/card/CardActionBar';
 
-export default function ReviewActionBar() {
-    return (
-        <div>ReviewActionBar</div>
-    )
+export default function ReviewActionBar({ reviewId, handleEdit, handleDelete, handleLike }) {
+    const actions = [];
+
+    if (handleEdit) {
+        actions.push({
+            onClick: () => handleEdit(reviewId),
+            icon: <EditIcon />,
+        });
+    }
+
+    if (handleDelete) {
+        actions.push({
+            onClick: () => handleDelete(reviewId),
+            icon: <DeleteIcon />,
+        });
+    }
+
+    actions.push({
+        onClick: () => handleLike(reviewId),
+        icon: <FavoriteIcon />,
+    });
+
+    return <CardActionBar actions={actions} />;
 }
