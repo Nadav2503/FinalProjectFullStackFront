bfaca4402234a3a1e99e0ab2c71399b4e6d31333
Nadav2503 <nadavivon.re@gmail.com>
2024-12-11 16:52:53 +0200

    correct the normalize

--- src/review/helpers/normalizeReview.js
+++ src/review/helpers/normalizeReview.js
@@ -1,8 +1,14 @@
-const normalizeReview = (review) => {
+const normalizeReview = (review, visitorId) => {
     return {
+        visitorId,  // Ensure visitorId is passed
         rating: Number(review.rating), // Ensure rating is a number
         comment: review.comment?.trim() || null, // Trim the comment and set null if empty
+        exhibitId: review.exhibitId || null,
+        animalId: review.animalId || null,
+        likes: review.likes || [],
+        date: new Date(),
     };
 };
+
 
 export default normalizeReview;
