cd1c93234e6ad63fa2f2e33a06ee1142e7a5bf64
Nadav2503 <nadavivon.re@gmail.com>
2024-12-11 15:14:30 +0200

    add use states for errors and review. +normalize before the update

--- src/review/hooks/useUpdateReview.js
+++ src/review/hooks/useUpdateReview.js
@@ -1,26 +1,33 @@
 import { useState, useCallback } from "react";
 import { updateReview } from "../../services/ReviewServiceApi";
 import { useSnack } from "../../providers/SnackbarProvider";
+import normalizeReview from "../helpers/normalizeReview";
 
 const useUpdateReview = () => {
+    const [review, setReview] = useState(null);
     const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
     const setSnack = useSnack();
 
     const handleUpdate = useCallback(async (id, updatedData) => {
         setLoading(true);
+        setError(null);
         try {
-            const response = await updateReview(id, updatedData); // API call
+            const normalizedReview = normalizeReview(updatedData);
+            const updatedReview = await updateReview(id, normalizedReview); // API call
+            setReview(updatedReview);
             setSnack("success", "Review updated successfully!");
-            return response;
+
         } catch (err) {
+            setError(err.message);
             setSnack("error", `Failed to update review: ${err.message}`);
-            throw err;
+
         } finally {
             setLoading(false);
         }
     }, [setSnack]);
 
-    return { handleUpdate, loading };
+    return { handleUpdate, loading, review, error };
 };
 
 export default useUpdateReview;
