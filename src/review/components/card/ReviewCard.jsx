07465c481ba07150f1d0e72e2c0164532619ce75
Nadav2503 <nadavivon.re@gmail.com>
2024-12-11 11:39:36 +0200

    create the review card with the combined components of review

--- src/review/components/card/ReviewCard.jsx
+++ src/review/components/card/ReviewCard.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Card from '../../../general/card/Card';
+import ReviewHeader from './ReviewHeader';
+import ReviewBody from './ReviewBody';
+import ReviewActionBar from './ReviewActionBar';
+import { CardActionArea } from '@mui/material';
 
-export default function ReviewCard() {
+export default function ReviewCard({ review, handleEdit, handleDelete, handleLike }) {
     return (
-        <div>ReviewCard</div>
-    )
+        <Card>
+            <ReviewHeader visitorId={review.visitorId} />
+            <CardActionArea>
+                <ReviewBody comment={review.comment} rating={review.rating} date={review.date} />
+            </CardActionArea>
+            <ReviewActionBar
+                reviewId={review.id}
+                handleEdit={handleEdit}
+                handleDelete={handleDelete}
+                handleLike={handleLike}
+            />
+        </Card>
+    );
 }
