2234f3159434bd950bb8a9dce04c773de1a9ec39
Nadav2503 <nadavivon.re@gmail.com>
2024-12-11 11:35:13 +0200

    move the content into content variable

--- src/review/components/card/ReviewBody.jsx
+++ src/review/components/card/ReviewBody.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Typography } from '@mui/material';
 
 export default function ReviewBody({ comment, rating, date }) {
-    return (
-        <div>
+    const content = (
+        <>
             <Typography variant="body1">{comment}</Typography> {/* Review Comment */}
             <Typography variant="body2">Rating: {rating}</Typography> {/* Rating */}
             <Typography variant="body2">{date}</Typography> {/* Review Date */}
-        </div>
+        </>
+    )
+    return (
+        content
     );
 }
