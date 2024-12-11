65ac22df843071045e0f5a07ec97828654cbeb29
Nadav2503 <nadavivon.re@gmail.com>
2024-12-11 12:13:02 +0200

    add reviews to props and add conditional rednering

--- src/review/components/ReviewFeedback.jsx
+++ src/review/components/ReviewFeedback.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import Reviews from "./Reviews";
+import Loader from "../../general/Loader";
+import Error from "../../general/Error";
 
 export default function ReviewFeedback({
     isLoading,
@@ -7,7 +9,9 @@ export default function ReviewFeedback({
     error,
 
 }) {
-
+    if (isLoading) return <Loader />;
+    if (error) return <Error errorMessage={error} />;
+    if (!reviews || reviews.length === 0) return <Error errorMessage="Oops...no reviews to display" />;
 
     return <></>;
 }
