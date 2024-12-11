8491b90173b2b9dc8a241a2f7b7f52b0992263d3
Nadav2503 <nadavivon.re@gmail.com>
2024-12-11 10:03:17 +0200

    create the function for like review

--- src/review/hooks/useLikeReview.js
+++ src/review/hooks/useLikeReview.js
@@ -6,7 +6,19 @@ const useLikeReview = () => {
     const [loading, setLoading] = useState(false);
     const setSnack = useSnack();
 
-
+    const handleLike = useCallback(async (id) => {
+        setLoading(true);
+        try {
+            const response = await likeReview(id); // API call
+            setSnack("success", "Review like/unlike successful!");
+            return response;
+        } catch (err) {
+            setSnack("error", `Failed to like/unlike review: ${err.message}`);
+            throw err;
+        } finally {
+            setLoading(false);
+        }
+    }, [setSnack]);
 
     return {};
 };
