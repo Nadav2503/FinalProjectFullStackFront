cd1acc973e18ce6a5fd881f2200d3cd490574e26
Nadav2503 <nadavivon.re@gmail.com>
2024-12-16 19:31:27 +0200

    create usew effect for gets all the visitor + andle resize for responsive

--- src/visitor/pages/AdminRMCPage.jsx
+++ src/visitor/pages/AdminRMCPage.jsx
@@ -9,6 +9,15 @@ export default function AdminRMCPage() {
     // State to handle the confirmation dialog
     const [openDialog, setOpenDialog] = useState(false);
     const [selectedVisitorId, setSelectedVisitorId] = useState(null);
+    useEffect(() => {
+        fetchVisitors();
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 900); // Set the threshold for mobile/tablet
+        };
+        handleResize(); // Check initial screen size
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, [fetchVisitors]);
     return (
         <div>AdminRMCPage</div>
     )
