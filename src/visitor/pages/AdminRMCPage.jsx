6cb2051b08b49ee88aca9387a06ac85b9f2f25c1
Nadav2503 <nadavivon.re@gmail.com>
2024-12-16 19:30:38 +0200

    add use states that will help me to achive the rmc page

--- src/visitor/pages/AdminRMCPage.jsx
+++ src/visitor/pages/AdminRMCPage.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
 export default function AdminRMCPage() {
+    const [isMobile, setIsMobile] = useState(false);
+    const { visitors, fetchVisitors } = useGetAllVisitors();
+    const { handleDeleteVisitor, loading: deleteLoading } = useDeleteVisitor();
+
+
+    // State to handle the confirmation dialog
+    const [openDialog, setOpenDialog] = useState(false);
+    const [selectedVisitorId, setSelectedVisitorId] = useState(null);
     return (
         <div>AdminRMCPage</div>
     )
