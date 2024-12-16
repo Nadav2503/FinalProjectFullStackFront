f8b774cbe3240da472d0c34f4b26c29fad5a6b69
Nadav2503 <nadavivon.re@gmail.com>
2024-12-16 19:39:56 +0200

    add visitor id header column

--- src/visitor/pages/AdminRMCPage.jsx
+++ src/visitor/pages/AdminRMCPage.jsx
@@ -61,7 +61,7 @@ export default function AdminRMCPage() {
                     }}
 
                 > <Typography variant="h6" sx={{ fontWeight: 'bold', marginBottom: 1 }}>
-                        {visitor.username}</Typography></Box>
+                        {visitor.username}</Typography> <Typography variant="body1">ID: {visitor._id}</Typography></Box>
             </Box>
 
         </Container>
