d70d259cefbedf05ab687df54e5b0bdabc21c7bd
Nadav2503 <nadavivon.re@gmail.com>
2024-12-17 08:18:18 +0200

    create zone array that contain all the possible locations of the exhibits

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -3,6 +3,15 @@ import PageHeader from '../general/PageHeader'
 import { Container } from '@mui/material'
 
 export default function MapPage() {
+    const zones = [
+        { name: "Africa", location: "Africa", color: "#FFCC00" },
+        { name: "Asia", location: "Asia", color: "#FF7043" },
+        { name: "Europe", location: "Europe", color: "#4DB6AC" },
+        { name: "North America", location: "North America", color: "#29B6F6" },
+        { name: "South America", location: "South America", color: "#81C784" },
+        { name: "Australia", location: "Australia", color: "#FFD54F" },
+        { name: "Antarctica", location: "Antarctica", color: "#B3E5FC" },
+    ];
     return (
         <Container>
             <PageHeader title="Zoo Map">
