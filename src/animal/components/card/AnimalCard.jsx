dc6945fbc9ebd09695da6bc6f0210e1013859af1
Nadav2503 <nadavivon.re@gmail.com>
2024-12-17 20:24:52 +0200

    create condition for the action bar itself in animal card

--- src/animal/components/card/AnimalCard.jsx
+++ src/animal/components/card/AnimalCard.jsx
@@ -7,7 +7,7 @@ import AnimalActionBar from './AnimalActionBar';
 import ROUTES from '../../../routers/routerModel';
 import { useNavigate } from 'react-router-dom';
 
-export default function AnimalCard({ animal, handleDelete, handleEditAnimal, handleFavoriteToggle, isLiked }) {
+export default function AnimalCard({ animal, handleDelete, handleEditAnimal, handleFavoriteToggle, isLiked, visitor }) {
     const navigate = useNavigate();
 
     const handleFavoriteClick = () => {
@@ -18,19 +18,26 @@ export default function AnimalCard({ animal, handleDelete, handleEditAnimal, han
         navigate(`${ROUTES.ANIMAL_INFO}/${animal._id}`);
     };
 
+    // Check if the visitor is Tier 1
+    const isTier1 = visitor?.membershipTier === 1;
+
     return (
         <Card>
             <AnimalHeader title={animal.name} image={animal.image} />
             <CardActionArea onClick={handleCardClick}>
                 <AnimalBody type={animal.type} gender={animal.gender} age={animal.age} />
             </CardActionArea>
-            <AnimalActionBar
-                animalId={animal._id}
-                handleDelete={handleDelete}
-                handleEditAnimal={handleEditAnimal}
-                handleFavoriteToggle={handleFavoriteClick}
-                isLiked={isLiked}
-            />
+            {/* Only show AnimalActionBar if the user is not Tier 1 */}
+            {!isTier1 && (
+                <AnimalActionBar
+                    animalId={animal._id}
+                    handleDelete={handleDelete}
+                    handleEditAnimal={handleEditAnimal}
+                    handleFavoriteToggle={handleFavoriteClick}
+                    isLiked={isLiked}
+                    visitor={visitor}
+                />
+            )}
         </Card>
     );
 }
