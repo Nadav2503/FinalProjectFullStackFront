84594fd5a5320d488be5cc491740a4abe8db1b5f
Nadav2503 <nadavivon.re@gmail.com>
2024-12-17 17:09:57 +0200

    remove the username from the navbar since i dont need it now

--- src/layout/navbar/right/RightHeader.jsx
+++ src/layout/navbar/right/RightHeader.jsx
@@ -1,31 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Box, Typography } from '@mui/material';
 import AvatarProfileImage from './Avatar';
 import AvatarMenu from './AvatarMenuAnchor';
 import useAnchor from '../useAnchor';
-import { useCurrentVisitor } from '../../../providers/VisitorProvider';
-import { getVisitorById } from '../../../services/VisitorServiceApi';
-
 export default function RightHeader() {
     const { anchorEl, handleAnchorClick, handleAnchorClose } = useAnchor();
-    const { visitor } = useCurrentVisitor(); // Access visitor context
-    const [username, setUsername] = useState(null);
-    useEffect(() => {
-        const fetchUsername = async () => {
-            if (visitor && visitor._id) { // Check visitor object
-                try {
-                    const visitorData = await getVisitorById(visitor._id); // Fetch visitor details
-                    setUsername(visitorData.username); // Update username state
-                } catch (error) {
-                    console.error('Error fetching visitor details:', error);
-                }
-            }
-        };
-
-        if (visitor && visitor._id) {
-            fetchUsername();
-        }
-    }, [visitor]); // Only rerun if visitor changes
 
     return (
         <Box
