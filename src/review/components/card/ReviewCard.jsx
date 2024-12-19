7fe9293ca922a20a0715977c653066f0a7cfe399
Nadav2503 <nadavivon.re@gmail.com>
2024-12-19 09:15:04 +0200

    apply the permission into review card

--- src/review/components/card/ReviewCard.jsx
+++ src/review/components/card/ReviewCard.jsx
@@ -5,7 +5,7 @@ import ReviewBody from "./ReviewBody";
 import ReviewActionBar from "./ReviewActionBar";
 import useGetVisitorById from "../../../visitor/hooks/useVisitorDataById";
 import { toggleLike, useReviewCardLogic } from "../../helpers/reviewHelpers";
-
+import { canEditOrDelete, canLike, isTier1 } from "../../../general/helpers/permission";
 
 export default function ReviewCard({
     review,
@@ -17,7 +17,6 @@ export default function ReviewCard({
 }) {
     const { fetchVisitorById, loading, error } = useGetVisitorById();
 
-    // Extract logic using the helper function
     const { isLiked, setIsLiked, username } = useReviewCardLogic(
         review,
         currentUserId,
@@ -31,24 +30,22 @@ export default function ReviewCard({
         toggleLike(review._id, handleLike, isLiked, setIsLiked);
     };
 
-    // Permissions logic (can later be moved to a permissions helper)
-    const isTier1 = visitor?.membershipTier === 1;
-    const canEditOrDelete = visitor?.isAdmin || review.visitorId === currentUserId;
-    const canLike = visitor?.membershipTier !== 1 || visitor?.isAdmin;
+    const editOrDeletePermission = canEditOrDelete(visitor, review.visitorId);
+    const likePermission = canLike(visitor);
 
     return (
         <Card>
-            <ReviewHeader visitorId={username} /> {/* Display username or "Anonymous" */}
+            <ReviewHeader visitorId={username} />
             <ReviewBody comment={review.comment} rating={review.rating} date={review.date} />
-            {!isTier1 && (
+            {!isTier1(visitor) && (
                 <ReviewActionBar
                     reviewId={review._id}
                     handleEdit={handleEdit}
                     handleDelete={handleDelete}
                     handleLike={handleLikeClick}
                     isLiked={isLiked}
-                    canEditOrDelete={canEditOrDelete}
-                    canLike={canLike}
+                    canEditOrDelete={editOrDeletePermission}
+                    canLike={likePermission}
                 />
             )}
         </Card>
